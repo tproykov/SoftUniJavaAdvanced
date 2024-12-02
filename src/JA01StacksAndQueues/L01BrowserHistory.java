f91401130352ba79b19b9a2c34751e68663a3244
tproykov <tproykov@gmail.com>
2024-12-02 11:15:24 +0000

    update

--- src/JA01StacksAndQueues/L01BrowserHistory.java
+++ src/JA01StacksAndQueues/L01BrowserHistory.java
@@ -1,4 +1,14 @@
 package JA01StacksAndQueues;
 
+import java.util.Scanner;
+
 public class L01BrowserHistory {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+
 }
--- src/JA01StacksAndQueues/L03DecimalToBinaryConverter.java
+++ src/JA01StacksAndQueues/L03DecimalToBinaryConverter.java
@@ -1,4 +1,15 @@
 package JA01StacksAndQueues;
 
+import java.util.Scanner;
+
 public class L03DecimalToBinaryConverter {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        
+
+    }
+
 }
