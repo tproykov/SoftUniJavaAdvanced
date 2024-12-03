3c67f85049243a5ba5210dede4eeacf0c6c74d11
tproykov <tproykov@gmail.com>
2024-12-03 21:32:10 +0000

    Update L02SimpleCalculator.java

--- src/JA01StacksAndQueues/L02SimpleCalculator.java
+++ src/JA01StacksAndQueues/L02SimpleCalculator.java
@@ -1,5 +1,7 @@
 package JA01StacksAndQueues;
 
+import java.util.ArrayDeque;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class L02SimpleCalculator {
@@ -8,8 +10,21 @@ public class L02SimpleCalculator {
 
         Scanner scanner = new Scanner(System.in);
 
+        String[] input = scanner.nextLine().split("\\s+");
 
+        ArrayDeque<String> stack = new ArrayDeque<>();
+        Collections.addAll(stack, input);
 
+        while (stack.size() > 1) {
+            int first = Integer.parseInt(stack.pop());
+            String operator = stack.pop();
+            int second = Integer.parseInt(stack.pop());
+
+            switch (operator) {
+                case "+" -> stack.push(String.valueOf(first + second));
+                case "-" -> stack.push(String.valueOf(first - second));
+            }
+        }
+        System.out.println(stack.pop());
     }
-
 }
