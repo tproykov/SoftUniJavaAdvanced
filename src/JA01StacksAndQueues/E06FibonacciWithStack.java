d110096873b9a0cafc1d784644a6ecfd1d290dc4
tproykov <tproykov@gmail.com>
2024-12-07 18:27:11 +0000

    update

--- src/JA01StacksAndQueues/E06FibonacciWithStack.java
+++ src/JA01StacksAndQueues/E06FibonacciWithStack.java
@@ -1,4 +1,35 @@
 package JA01StacksAndQueues;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Scanner;
+
 public class E06FibonacciWithStack {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        Deque<Long> stack = new ArrayDeque<>();
+
+        stack.push(0L);
+        stack.push(1L);
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        if (n < 2){
+            System.out.println(1);
+            return;
+        }
+
+        for (int i = 0; i < n; i++) {
+            long num1 = stack.pop();
+            long num2 = stack.pop();
+
+            stack.push(num1);
+            stack.push(num1 + num2);
+        }
+
+        System.out.println(stack.peek());
+    }
 }
