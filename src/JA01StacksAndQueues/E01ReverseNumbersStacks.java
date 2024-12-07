e54084e61279929bb7307ec84f406dc4c3e12ea7
tproykov <tproykov@gmail.com>
2024-12-07 13:58:54 +0000

    update

--- src/JA01StacksAndQueues/E01ReverseNumbersStacks.java
+++ src/JA01StacksAndQueues/E01ReverseNumbersStacks.java
@@ -1,4 +1,24 @@
 package JA01StacksAndQueues;
 
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class E01ReverseNumbersStacks {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int[] numbers = Arrays.stream(scanner.nextLine().split("\\s+"))
+                .mapToInt(Integer::parseInt).toArray();
+
+        ArrayDeque<Integer> stack = new ArrayDeque<>();
+
+        Arrays.stream(numbers).forEach(stack::push);
+
+        for (Integer number : stack) {
+            System.out.print(number + " ");
+        }
+    }
 }
