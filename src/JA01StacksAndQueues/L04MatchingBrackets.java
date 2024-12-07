5d96ff14ba9214b500d2fb2c48811b84a73adc61
tproykov <tproykov@gmail.com>
2024-12-07 09:55:27 +0000

    update

--- src/JA01StacksAndQueues/L04MatchingBrackets.java
+++ src/JA01StacksAndQueues/L04MatchingBrackets.java
@@ -1,5 +1,7 @@
 package JA01StacksAndQueues;
 
+import java.util.ArrayDeque;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class L04MatchingBrackets {
@@ -8,7 +10,24 @@ public class L04MatchingBrackets {
 
         Scanner scanner = new Scanner(System.in);
 
+        String input = scanner.nextLine();
 
+        ArrayDeque<Integer> stack = new ArrayDeque<>();
+
+        for (int i = 0; i < input.length(); i++) {
+
+            StringBuilder expression = new StringBuilder();
+
+            char character = input.charAt(i);
+
+            if (character == '(') {
+                stack.push(i);
+            }
+            else if (character == ')') {
+                int startIndex = stack.pop();
+                int endIndex = i + 1;
+                System.out.println(input.substring(startIndex, endIndex));
+            }
+        }
     }
-
 }
