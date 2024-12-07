5185958ea7bf2e59bec978a92aa9d7d91bca9bf4
tproykov <tproykov@gmail.com>
2024-12-07 09:58:51 +0000

    Update L05PrinterQueue.java

--- src/JA01StacksAndQueues/L05PrinterQueue.java
+++ src/JA01StacksAndQueues/L05PrinterQueue.java
@@ -1,5 +1,6 @@
 package JA01StacksAndQueues;
 
+import java.util.ArrayDeque;
 import java.util.Scanner;
 
 public class L05PrinterQueue {
@@ -7,6 +8,16 @@ public class L05PrinterQueue {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
+
+        ArrayDeque<String> queue = new ArrayDeque<>();
+
+        String filename;
+        while (!(filename = scanner.nextLine()).equals("print")) {
+
+            queue.offer(filename);
+        }
+
+        
 
 
 
