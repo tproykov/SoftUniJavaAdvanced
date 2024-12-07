cd44dfe3f880d9467634699a59a7764ac1388cf8
tproykov <tproykov@gmail.com>
2024-12-07 12:39:21 +0000

    Update L07MathPotato.java

--- src/JA01StacksAndQueues/L07MathPotato.java
+++ src/JA01StacksAndQueues/L07MathPotato.java
@@ -1,5 +1,7 @@
 package JA01StacksAndQueues;
 
+import java.util.ArrayDeque;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class L07MathPotato {
@@ -8,7 +10,32 @@ public class L07MathPotato {
 
         Scanner scanner = new Scanner(System.in);
 
+        String[] players = scanner.nextLine().split("\\s+");
+        ArrayDeque<String> queue = new ArrayDeque<>();
 
+        for (int i = 1; i < players.length; i++) {
+            queue.offer(players[i]);
+
+
+
+        }
+
+        int n = Integer.parseInt(scanner.nextLine());
+        int rotations = 0;
+
+        while (queue.size() > 1) {
+
+            String firstKid = queue.poll();
+
+            rotations++;
+
+            if (rotations % n == 0) {
+                System.out.println("Removed " + firstKid);
+            } else {
+                queue.offer(firstKid);
+            }
+        }
+
+        System.out.println("Last is " + queue.poll());
     }
-
 }
