14d3c88fa23193e6c5f2bd4cb39190a2d8edda46
tproykov <tproykov@gmail.com>
2024-12-08 13:04:13 +0000

    update

--- src/JA02MultidiensionalArrays/L05MaxSumOfSubmatrix.java
+++ src/JA02MultidiensionalArrays/L05MaxSumOfSubmatrix.java
@@ -1,4 +1,36 @@
 package JA02MultidiensionalArrays;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class L05MaxSumOfSubmatrix {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int[] matrixDimensions = Arrays.stream(scanner.nextLine().split(", "))
+                .mapToInt(Integer::parseInt).toArray();
+
+        int rows = matrixDimensions[0];
+        int cols = matrixDimensions[1];
+
+        int[][] matrix = new int[rows][cols];
+
+        populateIntegerMatrix(matrix, scanner);
+
+
+
+
+
+    }
+
+    private static void populateIntegerMatrix(int[][] matrix, Scanner scanner) {
+
+        for (int row = 0; row < matrix.length; row++) {
+            int[] currentRow = Arrays.stream(scanner.nextLine().split(", "))
+                    .mapToInt(Integer::parseInt).toArray();
+            matrix[row] = currentRow;
+        }
+    }
 }
--- src/JA02MultidiensionalArrays/L06PrintDiagonalsSquare.java
+++ src/JA02MultidiensionalArrays/L06PrintDiagonalsSquare.java
@@ -1,4 +1,51 @@
 package JA02MultidiensionalArrays;
 
+import java.util.Scanner;
+
 public class L06PrintDiagonalsSquare {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int matrixSize = Integer.parseInt(scanner.nextLine());
+
+        int[][] matrix = new int[matrixSize][matrixSize];
+
+        populateIntegerMatrix(matrix, scanner);
+
+        int[] primaryDiagonal = new int[matrixSize];
+        int[] secondaryDiagonal = new int[matrixSize];
+
+        for (int i = 0; i < matrixSize; i++) {
+            primaryDiagonal[i] = matrix[i][i];
+        }
+
+        int counter = 0;
+        for (int i = matrixSize -1; i >= 0; i--) {
+            secondaryDiagonal[counter] = matrix[i][counter];
+            counter++;
+        }
+
+        for (int number : primaryDiagonal) {
+            System.out.print(number + " ");
+        }
+
+        System.out.println();
+
+        for (int number : secondaryDiagonal) {
+            System.out.print(number + " ");
+        }
+    }
+
+
+    private static void populateIntegerMatrix(int[][] matrix, Scanner scanner) {
+
+        for (int row = 0; row < matrix.length; row++) {
+            for (int col = 0; col < matrix[row].length; col++) {
+                matrix[row][col] = scanner.nextInt();
+            }
+            scanner.nextLine();
+        }
+    }
 }
