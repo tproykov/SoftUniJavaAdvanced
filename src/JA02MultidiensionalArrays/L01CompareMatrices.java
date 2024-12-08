ebe5f42ecb91c028e59087be24eea2d056c235cb
tproykov <tproykov@gmail.com>
2024-12-08 11:05:56 +0000

    Update L01CompareMatrices.java

--- src/JA02MultidiensionalArrays/L01CompareMatrices.java
+++ src/JA02MultidiensionalArrays/L01CompareMatrices.java
@@ -1,4 +1,71 @@
 package JA02MultidiensionalArrays;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class L01CompareMatrices {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int[] dimensionsMatrix1 = Arrays.stream(scanner.nextLine().split("\\s+"))
+                .mapToInt(Integer::parseInt).toArray();
+
+        int rowsMatrix1 = dimensionsMatrix1[0];
+        int colsMatrix1 = dimensionsMatrix1[1];
+
+        int[][] matrix1 = new int[rowsMatrix1][colsMatrix1];
+
+        populateMatrix(matrix1, scanner);
+
+        int[] dimensionsMatrix2 = Arrays.stream(scanner.nextLine().split("\\s+"))
+                .mapToInt(Integer::parseInt).toArray();
+
+        int rowsMatrix2 = dimensionsMatrix2[0];
+        int colsMatrix2 = dimensionsMatrix2[1];
+
+        int[][] matrix2 = new int[rowsMatrix2][colsMatrix2];
+
+        populateMatrix(matrix2, scanner);
+
+        if (compareIntegerMatrices(matrix1, matrix2)) {
+            System.out.println("Yes");
+        }
+        else {
+            System.out.println("No");
+        }
+    }
+
+    private static void populateMatrix(int[][] matrix, Scanner scanner) {
+
+        for (int row = 0; row < matrix.length; row++) {
+            for (int col = 0; col < matrix[row].length; col++) {
+                matrix[row][col] = scanner.nextInt();
+            }
+        }
+    }
+
+    private static boolean compareIntegerMatrices(int[][] matrix1, int[][] matrix2) {
+
+        if (matrix1.length != matrix2.length) {
+            return false;
+        }
+
+        for (int row = 0; row < matrix1.length; row++) {
+            if (matrix1[row].length != matrix2[row].length) {
+                return false;
+            }
+        }
+
+        for (int row = 0; row < matrix1.length; row++) {
+            for (int col = 0; col < matrix1[row].length; col++) {
+                if (matrix1[row][col] != matrix2[row][col]) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
 }
