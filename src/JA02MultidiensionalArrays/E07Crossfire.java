f13eec5b53a3100cabb0e000aff8f12e7d137ac7
tproykov <tproykov@gmail.com>
2024-12-09 22:17:20 +0000

    update

--- src/JA02MultidiensionalArrays/E07Crossfire.java
+++ src/JA02MultidiensionalArrays/E07Crossfire.java
@@ -8,7 +8,6 @@ import java.util.Scanner;
 public class E07Crossfire {
 
     public static void main(String[] args) {
-
         Scanner scanner = new Scanner(System.in);
 
         int[] matrixDimensions = Arrays.stream(scanner.nextLine().split(" "))
@@ -17,33 +16,55 @@ public class E07Crossfire {
         int cols = matrixDimensions[1];
 
         List<List<Integer>> matrix = new ArrayList<>();
-
         populateTheMatrix(matrix, rows, cols);
 
         String command;
         while (!"Nuke it from orbit".equals(command = scanner.nextLine())) {
-
             int[] commandParts = Arrays.stream(command.split(" ")).mapToInt(Integer::parseInt).toArray();
             int targetRow = commandParts[0];
             int targetCol = commandParts[1];
             int radius = commandParts[2];
 
             for (int row = targetRow - radius; row <= targetRow + radius; row++) {
-                if
-
+                if (isInside(matrix, row, targetCol)) {
+                    matrix.get(row).set(targetCol, 0);
+                }
             }
 
+            for (int col = targetCol - radius; col <= targetCol + radius; col++) {
+                if (isInside(matrix, targetRow, col)) {
+                    matrix.get(targetRow).set(col, 0);
+                }
+            }
 
-
-
-
+            matrix = removeZeroesFromMatrix(matrix);
         }
 
         printTheMatrix(matrix);
     }
 
-    private static void populateTheMatrix(List<List<Integer>> matrix, int rows, int cols) {
+    private static List<List<Integer>> removeZeroesFromMatrix(List<List<Integer>> matrix) {
+        List<List<Integer>> newMatrix = new ArrayList<>();
 
+        for (List<Integer> row : matrix) {
+            List<Integer> newRow = new ArrayList<>();
+            for (Integer num : row) {
+                if (num != 0) {
+                    newRow.add(num);
+                }
+            }
+            if (!newRow.isEmpty()) {
+                newMatrix.add(newRow);
+            }
+        }
+        return newMatrix;
+    }
+
+    private static boolean isInside(List<List<Integer>> matrix, int row, int col) {
+        return row >= 0 && row < matrix.size() && col >= 0 && col < matrix.get(row).size();
+    }
+
+    private static void populateTheMatrix(List<List<Integer>> matrix, int rows, int cols) {
         for (int row = 0; row < rows; row++) {
             matrix.add(new ArrayList<>());
         }
--- src/temp.java
+++ src/temp.java
@@ -6,19 +6,7 @@ public class temp {
 
         Scanner scanner = new Scanner(System.in);
 
-        int[] matrixDimensions = Arrays.stream(scanner.nextLine().split(" "))
-                .mapToInt(Integer::parseInt).toArray();
-        int rows = matrixDimensions[0];
-        int cols = matrixDimensions[1];
 
-        List<List<Integer>> matrix = new ArrayList<>();
-
-        int counter = 1;
-        for (int row = 0; row < matrix.size(); row++) {
-            for (int col = 0; col < matrix.getFirst().size(); col++) {
-                matrix.get(row).add(counter++);
-            }
-        }
 
     }
 }
