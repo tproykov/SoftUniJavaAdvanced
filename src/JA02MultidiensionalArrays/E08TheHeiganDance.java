b14f2920e3e65f48d6598740aaa18f3e90252bfe
tproykov <tproykov@gmail.com>
2024-12-09 22:33:32 +0000

    Update E08TheHeiganDance.java

--- src/JA02MultidiensionalArrays/E08TheHeiganDance.java
+++ src/JA02MultidiensionalArrays/E08TheHeiganDance.java
@@ -1,4 +1,32 @@
 package JA02MultidiensionalArrays;
 
+import java.util.Scanner;
+
 public class E08TheHeiganDance {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        double damage = Double.parseDouble(scanner.nextLine());
+
+        String command = scanner.nextLine();
+        while (true) {
+
+            String[] tokens = command.split("\\s+");
+            String spell = tokens[0];
+            int row = Integer.parseInt(tokens[1]);
+            int col = Integer.parseInt(tokens[2]);
+
+
+
+
+
+            command = scanner.nextLine();
+        }
+
+
+
+    }
+
 }
