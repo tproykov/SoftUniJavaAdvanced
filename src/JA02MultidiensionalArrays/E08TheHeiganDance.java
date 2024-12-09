74d3717c49eb22e313a28672794eebbd9e7f1819
tproykov <tproykov@gmail.com>
2024-12-09 23:14:36 +0000

    Update E08TheHeiganDance.java

--- src/JA02MultidiensionalArrays/E08TheHeiganDance.java
+++ src/JA02MultidiensionalArrays/E08TheHeiganDance.java
@@ -11,16 +11,48 @@ public class E08TheHeiganDance {
         double damageToHeigan = Double.parseDouble(scanner.nextLine());
         int[] playerPosition = {8, 8};
         int[][] matrix = new int[15][15];
-        int playerStartingPoints = 18500;
-        int heiganStartingPoints = 3000000;
+        int playerPoints = 18500;
+        double heiganPoints = 3000000;
+        String winner = "";
 
         String command = scanner.nextLine();
         while (true) {
 
+            heiganPoints -= damageToHeigan;
+            if (heiganPoints <= 0) {
+                winner = "player";
+                break;
+            }
+
             String[] tokens = command.split("\\s+");
-            String spell = tokens[0];
-            int row = Integer.parseInt(tokens[1]);
-            int col = Integer.parseInt(tokens[2]);
+            String spellType = tokens[0];
+            int rowHit = Integer.parseInt(tokens[1]);
+            int colHit = Integer.parseInt(tokens[2]);
+
+            switch (spellType) {
+
+                case "Cloud" -> {
+
+                    for (int row = rowHit - 1; row >= rowHit + 1; row++) {
+                        for (int col = colHit - 1; col >= colHit + 1; col--) {
+
+
+
+
+                            
+                        }
+
+
+
+                    }
+
+
+
+
+                }
+
+                case "Eruption" -> {}
+            }
 
 
 
