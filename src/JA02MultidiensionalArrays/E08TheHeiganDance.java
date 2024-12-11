31252b6b2d5f028bf0a9da3213c9ff8dde5e1a40
tproykov <tproykov@gmail.com>
2024-12-11 22:17:17 +0000

    update

--- src/JA02MultidiensionalArrays/E08TheHeiganDance.java
+++ src/JA02MultidiensionalArrays/E08TheHeiganDance.java
@@ -1,6 +1,5 @@
 package JA02MultidiensionalArrays;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class E08TheHeiganDance {
@@ -10,120 +9,112 @@ public class E08TheHeiganDance {
         Scanner scanner = new Scanner(System.in);
 
         double damageToHeigan = Double.parseDouble(scanner.nextLine());
-        int[] playerPosition = {8, 8};
-        int[][] matrix = new int[15][15];
+        int[] playerPosition = {7, 7}; // Starting at center (0-based index)
         int playerPoints = 18500;
         double heiganPoints = 3000000;
-        String winner = "";
         boolean activeCloud = false;
+        String lastSpell = "";
 
+        
         String command = scanner.nextLine();
         while (true) {
+
+            heiganPoints -= damageToHeigan;
+            if (heiganPoints <= 0) {
+                printGameResult(heiganPoints, playerPoints, lastSpell, playerPosition);
+                return;
+            }
 
             if (activeCloud) {
                 playerPoints -= 3500;
                 activeCloud = false;
-            }
-
-            heiganPoints -= damageToHeigan;
-            if (heiganPoints <= 0) {
-                winner = "player";
-                break;
+                if (playerPoints <= 0) {
+                    printGameResult(heiganPoints, playerPoints, "Plague Cloud", playerPosition);
+                    return;
+                }
             }
 
             String[] tokens = command.split("\\s+");
             String spellType = tokens[0];
             int rowHit = Integer.parseInt(tokens[1]);
             int colHit = Integer.parseInt(tokens[2]);
+            lastSpell = spellType;
 
-            for (int row = rowHit - 1; row <= rowHit + 1; row++) {
+            if (isInDamageZone(playerPosition[0], playerPosition[1], rowHit, colHit)) {
+                boolean moved = tryToMove(playerPosition, rowHit, colHit);
 
-                for (int col = colHit - 1; col <= colHit + 1; col--) {
-
-                    if (isInside(matrix, row, col)) {
-                        matrix[row][col] = 1;
-                    }
-                }
-            }
-
-            printMatrix(matrix);
-
-            int playerCurrentRow = playerPosition[0];
-            int playerCurrentCol = playerPosition[1];
-            boolean takeTheHit = false;
-            if (matrix[playerCurrentRow][playerCurrentCol] == 1) {
-
-                if (isInside(matrix, playerCurrentRow - 1, playerCurrentCol)
-                        && matrix[playerCurrentRow - 1][playerCurrentCol] == 0) {
-
-                    playerPosition[0] = playerCurrentRow - 1;
-                }
-                else if (isInside(matrix, playerCurrentRow, playerCurrentCol + 1)
-                        && matrix[playerCurrentRow][playerCurrentCol + 1] == 0) {
-
-                    playerPosition[1] = playerCurrentCol + 1;
-                }
-                else if (isInside(matrix, playerCurrentRow + 1, playerCurrentCol)
-                        && matrix[playerCurrentRow + 1][playerCurrentCol] == 0) {
-
-                    playerPosition[0] = playerCurrentRow + 1;
-                }
-                else if (isInside(matrix, playerCurrentRow, playerCurrentCol - 1)
-                        && matrix[playerCurrentRow][playerCurrentCol - 1] == 0) {
-
-                    playerPosition[1] = playerCurrentCol - 1;
-                }
-                else {
-                    takeTheHit = true;
-                }
-            }
-
-            if (takeTheHit) {
-
-                switch (spellType) {
-
-                    case "Cloud" -> {
+                if (!moved) {
+                    if (spellType.equals("Cloud")) {
                         playerPoints -= 3500;
                         activeCloud = true;
-                    }
-
-                    case "Eruption" -> {
+                        if (playerPoints <= 0) {
+                            printGameResult(heiganPoints, playerPoints, "Plague Cloud", playerPosition);
+                            return;
+                        }
+                    } else {
                         playerPoints -= 6000;
+                        if (playerPoints <= 0) {
+                            printGameResult(heiganPoints, playerPoints, spellType, playerPosition);
+                            return;
+                        }
                     }
                 }
             }
 
-            if (playerPoints <= 0) {
-                winner = "heigan";
+            if (!scanner.hasNextLine()) {
                 break;
             }
-
-            resetMatrix(matrix);
             command = scanner.nextLine();
         }
 
-        System.out.println(winner + ": " + heiganPoints);
-        System.out.println(Arrays.toString(playerPosition));
+        printGameResult(heiganPoints, playerPoints, lastSpell, playerPosition);
     }
 
-    private static boolean isInside(int[][] matrix, int row, int col) {
-        return row >= 0 && row < matrix.length && col >= 0 && col < matrix.length;
+    private static boolean isInDamageZone(int playerRow, int playerCol, int hitRow, int hitCol) {
+        return Math.abs(playerRow - hitRow) <= 1 && Math.abs(playerCol - hitCol) <= 1;
     }
 
-    private static void resetMatrix(int[][] matrix) {
-        for (int row = 0; row < matrix.length; row++) {
-            for (int col = 0; col < matrix[row].length; col++) {
-                matrix[row][col] = 0;
-            }
+    private static boolean isInside(int row, int col) {
+        return row >= 0 && row < 15 && col >= 0 && col < 15;
+    }
+
+    private static boolean tryToMove(int[] playerPosition, int hitRow, int hitCol) {
+        if (canMoveTo(playerPosition[0] - 1, playerPosition[1], hitRow, hitCol)) {
+            playerPosition[0]--;
+            return true;
         }
+        if (canMoveTo(playerPosition[0], playerPosition[1] + 1, hitRow, hitCol)) {
+            playerPosition[1]++;
+            return true;
+        }
+        if (canMoveTo(playerPosition[0] + 1, playerPosition[1], hitRow, hitCol)) {
+            playerPosition[0]++;
+            return true;
+        }
+        if (canMoveTo(playerPosition[0], playerPosition[1] - 1, hitRow, hitCol)) {
+            playerPosition[1]--;
+            return true;
+        }
+        return false;
     }
 
-    private static void printMatrix(int[][] matrix) {
-        for (int[] ints : matrix) {
-            for (int anInt : ints) {
-                System.out.print(anInt + " ");
-            }
-            System.out.println();
+    private static boolean canMoveTo(int newRow, int newCol, int hitRow, int hitCol) {
+        return isInside(newRow, newCol) && !isInDamageZone(newRow, newCol, hitRow, hitCol);
+    }
+
+    private static void printGameResult(double heiganPoints, int playerPoints, String lastSpell, int[] position) {
+        if (heiganPoints <= 0) {
+            System.out.println("Heigan: Defeated!");
+        } else {
+            System.out.printf("Heigan: %.2f\n", heiganPoints);
         }
+
+        if (playerPoints <= 0) {
+            System.out.println("Player: Killed by " + lastSpell);
+        } else {
+            System.out.println("Player: " + playerPoints);
+        }
+
+        System.out.println("Final position: " + position[0] + ", " + position[1]);
     }
 }
