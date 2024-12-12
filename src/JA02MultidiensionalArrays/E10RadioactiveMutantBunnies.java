832670bfc319a0f2403b50a0a86ee58ab5346928
tproykov <tproykov@gmail.com>
2024-12-12 21:47:07 +0000

    Update E10RadioactiveMutantBunnies.java

--- src/JA02MultidiensionalArrays/E10RadioactiveMutantBunnies.java
+++ src/JA02MultidiensionalArrays/E10RadioactiveMutantBunnies.java
@@ -16,55 +16,98 @@ public class E10RadioactiveMutantBunnies {
 
         char[][] lair = new char[rows][columns];
 
+        int playerRowPosition = 0;
+        int playerColumnPosition = 0;
+        boolean playerDied = false;
+
         for (int i = 0; i < rows; i++) {
             String line = scanner.nextLine();
             for (int j = 0; j < columns; j++) {
-                lair[i][j] = line.charAt(j);
+                char character = line.charAt(j);
+                if (character == 'P') {
+                    playerRowPosition = i;
+                    playerColumnPosition = j;
+                }
+                lair[i][j] = character;
             }
         }
 
         char[] directions = scanner.nextLine().toCharArray();
 
-        for (int i = 0; i < directions.length; i++) {
+        for (char direction : directions) {
 
-            char direction = directions[i];
+            switch (direction) {
+                case 'R' -> playerColumnPosition++;
+                case 'L' -> playerColumnPosition--;
+                case 'U' -> playerRowPosition--;
+                case 'D' -> playerRowPosition++;
+            }
+
+            if (!isInside(lair, playerRowPosition, playerColumnPosition)) {
+                printLair(lair);
+                System.out.print("won: ");
+                System.out.print(playerRowPosition + " " + playerColumnPosition);
+                return;
+            }
+
+            if (steppedOnBunny(lair, playerRowPosition, playerColumnPosition)) {
+                printLair(lair);
+                System.out.print("dead: ");
+                System.out.print(playerRowPosition + " " + playerColumnPosition);
+                return;
+            }
 
             for (int row = 0; row < rows; row++) {
                 for (int col = 0; col < columns; col++) {
+
                     if (lair[row][col] == 'B') {
 
-                        int rowNextBunny = row;
-                        int colNextBunny = col;
-                        switch (direction) {
-                            case 'R' -> colNextBunny++;
-                            case 'L' -> colNextBunny--;
-                            case 'U' -> rowNextBunny--;
-                            case 'D' -> rowNextBunny++;
+                        for (int bunnyRow = row - 1; bunnyRow <= row + 1; bunnyRow++) {
+                            if (isInside(lair, bunnyRow, col)) {
+                                if (steppedOnPlayer(lair, bunnyRow, col)) {
+                                    playerDied = true;
+                                }
+                                lair[bunnyRow][col] = 'B';
+                            }
                         }
-
-                        if (isInside(lair, rowNextBunny, colNextBunny)) {
-                            lair[rowNextBunny][colNextBunny] = 'B';
+                        for (int bunnyCol = col - 1; bunnyCol <= col + 1; bunnyCol++) {
+                            if (isInside(lair, bunnyCol, row)) {
+                                if (steppedOnPlayer(lair, row, bunnyCol)) {
+                                    playerDied = true;
+                                }
+                                lair[row][bunnyCol] = 'B';
+                            }
                         }
+                    }
+                    
+                    if (playerDied) {
+                        printLair(lair);
+                        System.out.print("died: ");
+                        System.out.print(playerRowPosition + " " + playerColumnPosition);
                     }
                 }
             }
-
-
-
-
-
-
-
-
         }
-
-
-
     }
 
     private static boolean isInside(char[][] lairs, int row, int column) {
         return row >= 0 && row < lairs.length && column >= 0 && column < lairs[0].length;
     }
 
+    private static boolean steppedOnBunny(char[][] lair, int row, int column) {
+        return lair[row][column] == 'B';
+    }
 
+    private static boolean steppedOnPlayer(char[][] lair, int row, int column) {
+        return lair[row][column] == 'P';
+    }
+
+    private static void printLair(char[][] lair) {
+        for (char[] row : lair) {
+            for (char character : row) {
+                System.out.print(character + "");
+            }
+            System.out.println();
+        }
+    }
 }
