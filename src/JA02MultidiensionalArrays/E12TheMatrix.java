7795276826d4f23d2f80da68fd299dbe48c24cd5
tproykov <tproykov@gmail.com>
2024-12-13 23:08:18 +0000

    Update E12TheMatrix.java

--- src/JA02MultidiensionalArrays/E12TheMatrix.java
+++ src/JA02MultidiensionalArrays/E12TheMatrix.java
@@ -1,4 +1,17 @@
 package JA02MultidiensionalArrays;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class E12TheMatrix {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int[] matrixDimensions = Arrays.stream(scanner.nextLine().split(" "))
+                .mapToInt(Integer::parseInt).toArray();
+
+    }
+
 }
