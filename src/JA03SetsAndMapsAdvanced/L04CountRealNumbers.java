bb094deef698d58ee23c8e0bcccd7bdcd62a7ebe
tproykov <tproykov@gmail.com>
2024-12-14 13:30:48 +0000

    update

--- src/JA03SetsAndMapsAdvanced/L04CountRealNumbers.java
+++ src/JA03SetsAndMapsAdvanced/L04CountRealNumbers.java
@@ -1,4 +1,24 @@
 package JA03SetsAndMapsAdvanced;
 
+import java.util.*;
+
 public class L04CountRealNumbers {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        double[] realNumbers = Arrays.stream(scanner.nextLine().split("\\s+"))
+                .mapToDouble(Double::parseDouble).toArray();
+
+        Map<Double, Integer> map = new LinkedHashMap<>();
+
+        for (double realNumber : realNumbers) {
+            map.put(realNumber, map.getOrDefault(realNumber, 0) + 1);
+        }
+
+        for (Map.Entry<Double, Integer> entry : map.entrySet()) {
+            System.out.printf("%.1f -> %d\n", entry.getKey(), entry.getValue());
+        }
+    }
 }
