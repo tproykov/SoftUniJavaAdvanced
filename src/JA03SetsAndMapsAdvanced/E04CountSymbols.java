3ba1d080da57f6407c8d7775824435d176203b91
tproykov <tproykov@gmail.com>
2024-12-14 15:37:42 +0000

    update

--- src/JA03SetsAndMapsAdvanced/E04CountSymbols.java
+++ src/JA03SetsAndMapsAdvanced/E04CountSymbols.java
@@ -1,4 +1,33 @@
 package JA03SetsAndMapsAdvanced;
 
+import java.util.Map;
+import java.util.Scanner;
+import java.util.TreeMap;
+
 public class E04CountSymbols {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+
+        Map<Character, Integer> symbols = new TreeMap<>();
+
+        for (int i = 0; i < input.length(); i++) {
+
+            char character = input.charAt(i);
+
+            if (!symbols.containsKey(character)) {
+                symbols.put(character, 1);
+            }
+            else {
+                symbols.put(character, symbols.get(character) + 1);
+            }
+        }
+
+        for (Map.Entry<Character, Integer> entry : symbols.entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue() + " time/s");
+        }
+    }
 }
