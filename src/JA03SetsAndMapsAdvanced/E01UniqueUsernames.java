90c4c86962f39ce512c18fde913625ea6d651f44
tproykov <tproykov@gmail.com>
2024-12-14 15:07:02 +0000

    update

--- src/JA03SetsAndMapsAdvanced/E01UniqueUsernames.java
+++ src/JA03SetsAndMapsAdvanced/E01UniqueUsernames.java
@@ -1,4 +1,26 @@
 package JA03SetsAndMapsAdvanced;
 
+import java.util.LinkedHashSet;
+import java.util.Scanner;
+import java.util.Set;
+
 public class E01UniqueUsernames {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        Set<String> usernames = new LinkedHashSet<>();
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        for (int i = 0; i < n; i++) {
+            String username = scanner.nextLine();
+            usernames.add(username);
+        }
+
+        for (String username : usernames) {
+            System.out.println(username);
+        }
+    }
 }
