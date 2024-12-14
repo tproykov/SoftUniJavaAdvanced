0abb21e08351c213e9d9da54fbf0126bd741147c
tproykov <tproykov@gmail.com>
2024-12-14 15:31:57 +0000

    update

--- src/JA03SetsAndMapsAdvanced/E03PeriodicTable.java
+++ src/JA03SetsAndMapsAdvanced/E03PeriodicTable.java
@@ -1,4 +1,29 @@
 package JA03SetsAndMapsAdvanced;
 
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.TreeSet;
+
 public class E03PeriodicTable {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        Set<String> elements = new TreeSet<>();
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        for (int i = 0; i < n; i++) {
+
+            String[] compound = scanner.nextLine().split(" ");
+
+            elements.addAll(Arrays.asList(compound));
+        }
+
+        for (String element : elements) {
+            System.out.print(element + " ");
+        }
+    }
 }
