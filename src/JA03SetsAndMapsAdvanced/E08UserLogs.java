547b30743c2f643bcdeb444fc753a52cd8f85da3
tproykov <tproykov@gmail.com>
2024-12-14 16:43:03 +0000

    Update E08UserLogs.java

--- src/JA03SetsAndMapsAdvanced/E08UserLogs.java
+++ src/JA03SetsAndMapsAdvanced/E08UserLogs.java
@@ -1,4 +1,15 @@
 package JA03SetsAndMapsAdvanced;
 
+import java.util.Scanner;
+
 public class E08UserLogs {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        
+
+    }
+
 }
