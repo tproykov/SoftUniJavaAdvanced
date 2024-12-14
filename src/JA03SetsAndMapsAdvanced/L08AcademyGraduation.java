79b24089fe42670fa9aa5c5e7237cf3ceefe8824
tproykov <tproykov@gmail.com>
2024-12-14 14:45:45 +0000

    update

--- src/JA03SetsAndMapsAdvanced/L08AcademyGraduation.java
+++ src/JA03SetsAndMapsAdvanced/L08AcademyGraduation.java
@@ -1,6 +1,6 @@
 package JA03SetsAndMapsAdvanced;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class L08AcademyGraduation {
 
@@ -8,7 +8,27 @@ public class L08AcademyGraduation {
 
         Scanner scanner = new Scanner(System.in);
 
+        int n = Integer.parseInt(scanner.nextLine());
 
+        Map<String, Double> studentsGrades = new TreeMap<>();
+
+        for (int i = 0; i < n; i++) {
+
+            String studentName = scanner.nextLine();
+            double[] grades = Arrays.stream(scanner.nextLine().split("\\s+"))
+                    .mapToDouble(Double::parseDouble).toArray();
+
+            double sum = 0;
+            for (double grade : grades) {
+                sum += grade;
+            }
+            double average = sum / grades.length;
+            studentsGrades.put(studentName, average);
+        }
+
+        for (Map.Entry<String, Double> entry : studentsGrades.entrySet()) {
+
+            System.out.println(entry.getKey() + " is graduated with " + entry.getValue());
+        }
     }
-
 }
