b71868cc13f5e56782f876e700fb28ceff80985b
tproykov <tproykov@gmail.com>
2024-12-14 12:32:40 +0000

    update

--- src/JA03SetsAndMapsAdvanced/L02SoftUniParty.java
+++ src/JA03SetsAndMapsAdvanced/L02SoftUniParty.java
@@ -1,4 +1,49 @@
 package JA03SetsAndMapsAdvanced;
 
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Scanner;
+import java.util.Set;
+
 public class L02SoftUniParty {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        Set<String> vipGuests = new LinkedHashSet<>();
+        Set<String> guests = new LinkedHashSet<>();
+
+        String input1;
+        while (!(input1 = scanner.nextLine()).equals("PARTY")) {
+
+            if (Character.isDigit(input1.charAt(0))) {
+                vipGuests.add(input1);
+            }
+            else{
+                guests.add(input1);
+            }
+        }
+
+        String input2;
+        while (!(input2 = scanner.nextLine()).equals("END")) {
+
+            if (Character.isDigit(input2.charAt(0))) {
+                vipGuests.remove(input2);
+            }
+            else{
+                guests.remove(input2);
+            }
+        }
+
+        System.out.println(vipGuests.size() + guests.size());
+
+        for (String vipGuest : vipGuests) {
+            System.out.println(vipGuest);
+        }
+
+        for (String guest : guests) {
+            System.out.println(guest);
+        }
+    }
 }
