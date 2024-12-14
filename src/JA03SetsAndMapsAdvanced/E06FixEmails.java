f298a79f998eede6e92e05cf9302cd1fe190a301
tproykov <tproykov@gmail.com>
2024-12-14 16:03:14 +0000

    update

--- src/JA03SetsAndMapsAdvanced/E06FixEmails.java
+++ src/JA03SetsAndMapsAdvanced/E06FixEmails.java
@@ -1,4 +1,35 @@
 package JA03SetsAndMapsAdvanced;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class E06FixEmails {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        Map<String, String> contactEmails = new LinkedHashMap<>();
+
+        String command;
+        while (!"stop".equals(command = scanner.nextLine())) {
+
+            String email = scanner.nextLine();
+
+            String[] emailParts = email.split("\\.");
+            if (emailParts[emailParts.length - 1].equals("com")
+                    || emailParts[emailParts.length - 1].equals("us")
+                    || emailParts[emailParts.length - 1].equals("uk")) {
+
+                continue;
+            }
+
+            contactEmails.put(command, email);
+        }
+
+        for (Map.Entry<String, String> entry : contactEmails.entrySet()) {
+            System.out.println(entry.getKey() + " -> " + entry.getValue());
+        }
+    }
 }
