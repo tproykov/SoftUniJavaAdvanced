4c4cbde3e80859fafb90b59076b105c10701129f
tproykov <tproykov@gmail.com>
2024-12-15 23:07:20 +0000

    update

--- src/JA04StreamsFilesDirectories/L07ListFiles.java
+++ src/JA04StreamsFilesDirectories/L07ListFiles.java
@@ -1,4 +1,18 @@
 package JA04StreamsFilesDirectories;
 
+import java.io.File;
+
 public class L07ListFiles {
+
+    public static void main(String[] args) {
+
+        File folder = new File("C:\\Users\\todor\\Zoho WorkDrive (Dr Todor Proykov)\\My Folders\\GitHub Repositories\\SoftUniJavaAdvanced\\src\\JA04StreamsFilesDirectories\\resources");
+
+        File[] files = folder.listFiles();
+        for (File file : files) {
+            if (!file.isDirectory()) {
+                System.out.printf("%s: [%d]\n", file.getName(), file.length());
+            }
+        }
+    }
 }
