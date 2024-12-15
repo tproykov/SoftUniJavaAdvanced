1100bd88f9bc059d69b16753288cee4fcbe1fc82
tproykov <tproykov@gmail.com>
2024-12-15 23:02:55 +0000

    update

--- src/JA04StreamsFilesDirectories/L06SortLines.java
+++ src/JA04StreamsFilesDirectories/L06SortLines.java
@@ -1,4 +1,23 @@
 package JA04StreamsFilesDirectories;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Collections;
+import java.util.List;
+
 public class L06SortLines {
+
+    public static void main(String[] args) throws IOException {
+
+        String path = "C:\\Users\\todor\\Zoho WorkDrive (Dr Todor Proykov)\\My Folders\\GitHub Repositories\\SoftUniJavaAdvanced\\src\\JA04StreamsFilesDirectories\\resources\\input.txt";
+
+        List<String> lines = Files.readAllLines(Path.of(path));
+
+        Collections.sort(lines);
+
+        for (String line : lines) {
+            System.out.println(line);
+        }
+    }
 }
