7175da36701211891cee56cc099a9f3d10f1b7ce
tproykov <tproykov@gmail.com>
2024-12-15 01:09:32 +0000

    Update E12SerbianUnleashed.java

--- src/JA03SetsAndMapsAdvanced/E12SerbianUnleashed.java
+++ src/JA03SetsAndMapsAdvanced/E12SerbianUnleashed.java
@@ -7,7 +7,6 @@ import java.util.regex.Pattern;
 public class E12SerbianUnleashed {
 
     public static void main(String[] args) {
-
         Scanner scanner = new Scanner(System.in);
 
         LinkedHashMap<String, LinkedHashMap<String, Integer>> venuesSingersRevenue = new LinkedHashMap<>();
@@ -15,12 +14,11 @@ public class E12SerbianUnleashed {
         String command;
         while (!"End".equals(command = scanner.nextLine())) {
 
-            String regex = "(?<singer>[A-Z][a-z]+\\s*[A-Z]*[a-z]*)\\s@(?<venue>[A-Z][a-z]+\\s*[A-Z]*[a-z]*)\\s(?<price>\\d+)\\s(?<count>\\d+)";
+            String regex = "^(?<singer>[A-Za-z]+(\\s[A-Za-z]+){0,2})\\s@(?<venue>[A-Za-z]+(\\s[A-Za-z]+){0,2})\\s(?<price>\\d+)\\s(?<count>\\d+)$";
             Pattern pattern = Pattern.compile(regex);
             Matcher matcher = pattern.matcher(command);
 
-            while (matcher.find()) {
-
+            if (matcher.find()) {
                 String singer = matcher.group("singer");
                 String venue = matcher.group("venue");
                 int price = Integer.parseInt(matcher.group("price"));
@@ -39,17 +37,14 @@ public class E12SerbianUnleashed {
     }
 
     private static void printOutput(LinkedHashMap<String, LinkedHashMap<String, Integer>> map) {
-
         for (Map.Entry<String, LinkedHashMap<String, Integer>> entry : map.entrySet()) {
-
             System.out.println(entry.getKey());
 
             LinkedHashMap<String, Integer> singersRevenue = entry.getValue();
 
             List<Map.Entry<String, Integer>> sortedList = new ArrayList<>(singersRevenue.entrySet());
 
-            sortedList.sort((e1, e2) -> e2.getValue()
-                    .compareTo(e1.getValue()));
+            sortedList.sort((e1, e2) -> e2.getValue().compareTo(e1.getValue()));
 
             for (Map.Entry<String, Integer> singerEntry : sortedList) {
                 System.out.println("#  " + singerEntry.getKey() + " -> " + singerEntry.getValue());
