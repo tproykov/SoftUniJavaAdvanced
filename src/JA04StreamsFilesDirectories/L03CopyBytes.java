c2a25d63c3af9ea60a037c766cba73873dc6a7eb
tproykov <tproykov@gmail.com>
2024-12-15 12:01:14 +0000

    update

--- src/JA04StreamsFilesDirectories/L03CopyBytes.java
+++ src/JA04StreamsFilesDirectories/L03CopyBytes.java
@@ -1,4 +1,32 @@
 package JA04StreamsFilesDirectories;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class L03CopyBytes {
+
+    public static void main(String[] args) throws IOException {
+
+        String pathToRead = "C:\\Users\\todor\\Zoho WorkDrive (Dr Todor Proykov)\\My Folders\\GitHub Repositories\\SoftUniJavaAdvanced\\src\\JA04StreamsFilesDirectories\\resources\\input.txt";
+        FileInputStream fileInputStream = new FileInputStream(pathToRead);
+
+        String pathToWrite = "C:\\Users\\todor\\Zoho WorkDrive (Dr Todor Proykov)\\My Folders\\GitHub Repositories\\SoftUniJavaAdvanced\\src\\JA04StreamsFilesDirectories\\resources\\03.CopyBytesOutput.txt";
+        FileOutputStream fileOutputStream = new FileOutputStream(pathToWrite);
+
+        int oneByte = fileInputStream.read();
+        while (oneByte >= 0) {
+            if (oneByte == 32 || oneByte == 10) {
+                fileOutputStream.write(oneByte);
+            } else {
+                String digits = String.valueOf(oneByte);
+                for (int i = 0; i < digits.length(); i++) {
+                    fileOutputStream.write(digits.charAt(i));
+                }
+            }
+            oneByte = fileInputStream.read();
+        }
+
+        fileOutputStream.close();
+    }
 }
