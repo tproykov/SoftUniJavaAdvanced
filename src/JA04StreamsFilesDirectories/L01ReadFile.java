ba458e8fe1da4e49ea428c35147225997a6e4685
tproykov <tproykov@gmail.com>
2024-12-15 01:29:05 +0000

    Update L01ReadFile.java

--- src/JA04StreamsFilesDirectories/L01ReadFile.java
+++ src/JA04StreamsFilesDirectories/L01ReadFile.java
@@ -1,4 +1,15 @@
 package JA04StreamsFilesDirectories;
 
+import java.util.Scanner;
+
 public class L01ReadFile {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        
+
+    }
+
 }
