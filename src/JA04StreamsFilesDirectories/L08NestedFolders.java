1c9a52da63a377bcb6bccf1bb5036702d69ca8ce
tproykov <tproykov@gmail.com>
2024-12-15 23:24:12 +0000

    update

--- src/JA04StreamsFilesDirectories/L08NestedFolders.java
+++ src/JA04StreamsFilesDirectories/L08NestedFolders.java
@@ -1,4 +1,34 @@
 package JA04StreamsFilesDirectories;
 
+import java.io.File;
+import java.util.ArrayDeque;
+import java.util.Queue;
+
 public class L08NestedFolders {
+    public static void main(String[] args) {
+        File folder = new File("C:\\Users\\todor\\Zoho WorkDrive (Dr Todor Proykov)\\My Folders\\GitHub Repositories\\SoftUniJavaAdvanced\\src\\JA04StreamsFilesDirectories\\resources");
+
+        int folderCount = 0;
+        Queue<File> folders = new ArrayDeque<>();
+        folders.offer(folder);
+
+        while (!folders.isEmpty()) {
+            File currentFolder = folders.poll();
+            if (currentFolder.isDirectory()) {
+                System.out.println(currentFolder.getName());
+                folderCount++;
+
+                File[] nestedFolders = currentFolder.listFiles();
+                if (nestedFolders != null) {
+                    for (File file : nestedFolders) {
+                        if (file.isDirectory()) {
+                            folders.offer(file);
+                        }
+                    }
+                }
+            }
+        }
+
+        System.out.println(folderCount + " folders");
+    }
 }
