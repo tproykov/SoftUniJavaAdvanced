9e2a5d190c45270cd5772a45780410d453b5f2a8
tproykov <tproykov@gmail.com>
2024-12-15 22:54:25 +0000

    update

--- src/JA04StreamsFilesDirectories/L05WriteEveryThirdLine.java
+++ src/JA04StreamsFilesDirectories/L05WriteEveryThirdLine.java
@@ -1,4 +1,33 @@
 package JA04StreamsFilesDirectories;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class L05WriteEveryThirdLine {
+
+    public static void main(String[] args) throws FileNotFoundException {
+
+        String path = "C:\\Users\\todor\\Zoho WorkDrive (Dr Todor Proykov)\\My Folders\\GitHub Repositories\\SoftUniJavaAdvanced\\src\\JA04StreamsFilesDirectories\\resources\\input.txt";
+
+        FileInputStream fileInputStream = new FileInputStream(path);
+        Scanner scanner = new Scanner(fileInputStream);
+
+        List<String> lines = new ArrayList<>();
+        while (scanner.hasNextLine()) {
+
+            lines.add(scanner.nextLine());
+
+            // scanner.nextLine();
+        }
+
+        for (int i = 0; i < lines.size(); i++) {
+
+            if ((i + 1) % 3 == 0) {
+                System.out.println(lines.get(i));
+            }
+        }
+    }
 }
