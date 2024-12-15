377424bd3d2c3f7a6475c7bb11686269682c5f3d
tproykov <tproykov@gmail.com>
2024-12-15 10:58:56 +0000

    update

--- src/JA04StreamsFilesDirectories/L01ReadFile.java
+++ src/JA04StreamsFilesDirectories/L01ReadFile.java
@@ -1,15 +1,18 @@
 package JA04StreamsFilesDirectories;
 
-import java.util.Scanner;
+import java.io.FileInputStream;
+import java.io.IOException;
 
 public class L01ReadFile {
+    public static void main(String[] args) throws IOException {
+        String path = "C:\\Users\\todor\\Zoho WorkDrive (Dr Todor Proykov)\\My Folders\\GitHub Repositories\\SoftUniJavaAdvanced\\src\\JA04StreamsFilesDirectories\\resources\\input.txt";
 
-    public static void main(String[] args) {
+        FileInputStream fileStream = new FileInputStream(path);
 
-        Scanner scanner = new Scanner(System.in);
-
-
-
+        int oneByte = fileStream.read();
+        while (oneByte >= 0) {
+            System.out.print(Integer.toBinaryString(oneByte) + " ");
+            oneByte = fileStream.read();
+        }
     }
-
 }
