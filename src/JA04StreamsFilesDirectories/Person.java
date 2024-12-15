f7fccc5a8d3bed76b6af5d302db5a7c3d05fc9c1
tproykov <tproykov@gmail.com>
2024-12-15 23:54:43 +0000

    update

--- src/JA04StreamsFilesDirectories/Person.java
+++ src/JA04StreamsFilesDirectories/Person.java
@@ -3,7 +3,7 @@ package JA04StreamsFilesDirectories;
 import java.io.Serializable;
 
 public class Person implements Serializable {
-    private static final long serialVersionUID = 1L; // Add this line
+    private static final long serialVersionUID = 1L;
 
     private String name;
     private int age;
