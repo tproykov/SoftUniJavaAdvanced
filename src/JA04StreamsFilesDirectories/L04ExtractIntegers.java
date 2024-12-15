e75eb6d8ffd863ee6e9b13c30763f7de9ca73a18
tproykov <tproykov@gmail.com>
2024-12-15 22:47:36 +0000

    update

--- src/JA04StreamsFilesDirectories/L04ExtractIntegers.java
+++ src/JA04StreamsFilesDirectories/L04ExtractIntegers.java
@@ -1,8 +1,23 @@
 package JA04StreamsFilesDirectories;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class L04ExtractIntegers {
 
+    public static void main(String[] args) throws IOException {
 
+        String path = "C:\\Users\\todor\\Zoho WorkDrive (Dr Todor Proykov)\\My Folders\\GitHub Repositories\\SoftUniJavaAdvanced\\src\\JA04StreamsFilesDirectories\\resources\\input.txt";
 
+        FileInputStream fileInputStream = new FileInputStream(path);
+        Scanner scanner = new Scanner(fileInputStream);
 
+        while (scanner.hasNextLine()) {
+            if (scanner.hasNextInt()) {
+                System.out.println(scanner.nextInt());
+            }
+            scanner.next();
+        }
+    }
 }
