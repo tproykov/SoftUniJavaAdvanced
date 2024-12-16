c43ef987d9fa3363538f10aac6849c4cf806bc94
tproykov <tproykov@gmail.com>
2024-12-16 17:59:23 +0000

    Update E08GetFolderSize.java

--- src/JA04StreamsFilesDirectories/E08GetFolderSize.java
+++ src/JA04StreamsFilesDirectories/E08GetFolderSize.java
@@ -1,4 +1,27 @@
 package JA04StreamsFilesDirectories;
 
+import java.io.File;
+
 public class E08GetFolderSize {
+
+    public static void main(String[] args) {
+
+        String path = "src/JA04StreamsFilesDirectories/resources";
+
+        File directory = new File(path);
+
+        int totalSize = 0;
+        if (directory.isDirectory()) {
+            File[] files = directory.listFiles();
+
+            assert files != null;
+            for (File file : files) {
+                if (!file.isDirectory()) {
+                    totalSize += (int) file.length();
+                }
+            }
+        }
+
+        System.out.println(totalSize);
+    }
 }
