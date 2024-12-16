42605863a2379df355c9fc4c6358a6e22732d54c
tproykov <tproykov@gmail.com>
2024-12-16 18:06:01 +0000

    update

--- src/JA04StreamsFilesDirectories/E09CopyAPicture.java
+++ src/JA04StreamsFilesDirectories/E09CopyAPicture.java
@@ -1,4 +1,26 @@
 package JA04StreamsFilesDirectories;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 public class E09CopyAPicture {
+
+    public static void main(String[] args) {
+
+        String inputPath = "src/JA04StreamsFilesDirectories/resources/face2.jpg";
+        String outputPath = "src/JA04StreamsFilesDirectories/resources/copy-face2.jpg";
+
+        try (InputStream fis = Files.newInputStream(Path.of(inputPath));
+             OutputStream ous = Files.newOutputStream(Path.of(outputPath))
+        ) {
+            byte[] inputBytes = fis.readAllBytes();
+            ous.write(inputBytes);
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
