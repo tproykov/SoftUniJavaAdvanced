163074480b4dc3446cc28dacbdc003603db2d345
tproykov <tproykov@gmail.com>
2024-12-16 17:47:17 +0000

    Update E06WordCount.java

--- src/JA04StreamsFilesDirectories/E06WordCount.java
+++ src/JA04StreamsFilesDirectories/E06WordCount.java
@@ -20,37 +20,46 @@ public class E06WordCount {
         ) {
 
             List<String> wordList = new ArrayList<>();
+
             String line = wordReader.readLine();
+
             while (line != null) {
+
                 String[] words = line.split(" ");
                 wordList.addAll(Arrays.asList(words));
+                
                 line = wordReader.readLine();
             }
 
-            // Create a map to store word frequencies
+
             Map<String, Integer> wordFrequency = new HashMap<>();
+
             for (String word : wordList) {
                 wordFrequency.put(word.toLowerCase(), 0);
             }
 
-            // Read the text file and count word frequencies
+
             String textLine = textReader.readLine();
             while (textLine != null) {
+
                 String[] textWords = textLine.split("\\W+"); // Split by non-word characters
+
                 for (String textWord : textWords) {
+
                     String lowercaseWord = textWord.toLowerCase();
+
                     if (wordFrequency.containsKey(lowercaseWord)) {
                         wordFrequency.put(lowercaseWord, wordFrequency.get(lowercaseWord) + 1);
                     }
                 }
+
                 textLine = textReader.readLine();
             }
 
-            // Sort words by frequency in descending order
             List<Map.Entry<String, Integer>> sortedEntries = new ArrayList<>(wordFrequency.entrySet());
             sortedEntries.sort((e1, e2) -> e2.getValue().compareTo(e1.getValue()));
 
-            // Write results to output file
+
             for (Map.Entry<String, Integer> entry : sortedEntries) {
                 bufferedWriter.write(entry.getKey() + " - " + entry.getValue());
                 bufferedWriter.newLine();
