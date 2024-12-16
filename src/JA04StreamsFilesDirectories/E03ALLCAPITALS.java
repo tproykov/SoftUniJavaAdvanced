c1d1fdc344ecc6cc2bb672f80e7717f1a0bcbdd8
tproykov <tproykov@gmail.com>
2024-12-16 16:59:41 +0000

    update

--- src/JA04StreamsFilesDirectories/E03ALLCAPITALS.java
+++ src/JA04StreamsFilesDirectories/E03ALLCAPITALS.java
@@ -1,4 +1,34 @@
 package JA04StreamsFilesDirectories;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Scanner;
+
 public class E03ALLCAPITALS {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        String input = "src/JA04StreamsFilesDirectories/resources/input1.txt";
+
+        String output = "src/JA04StreamsFilesDirectories/resources/output.txt";
+
+        try (BufferedReader bufferedReader = Files.newBufferedReader(Path.of(input));
+             BufferedWriter bufferedWriter = Files.newBufferedWriter(Path.of(output))
+        ) {
+
+            String line = bufferedReader.readLine();
+            while (line != null) {
+                bufferedWriter.write(line.toUpperCase());
+                bufferedWriter.newLine();
+                line = bufferedReader.readLine();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
