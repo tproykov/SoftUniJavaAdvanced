f7761801f7044ce02465034c2524d478bbeb0292
tproykov <tproykov@gmail.com>
2024-12-16 17:53:52 +0000

    update

--- src/JA04StreamsFilesDirectories/E07MergeTwoFiles.java
+++ src/JA04StreamsFilesDirectories/E07MergeTwoFiles.java
@@ -1,4 +1,41 @@
 package JA04StreamsFilesDirectories;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 public class E07MergeTwoFiles {
+
+    public static void main(String[] args) {
+
+        String inputOne = "src/JA04StreamsFilesDirectories/resources/inputOne.txt";
+        String inputTwo = "src/JA04StreamsFilesDirectories/resources/inputTwo.txt";
+        String output = "src/JA04StreamsFilesDirectories/resources/output.txt";
+
+        try (BufferedReader reader1 = Files.newBufferedReader(Path.of(inputOne));
+             BufferedReader reader2 = Files.newBufferedReader(Path.of(inputTwo));
+             BufferedWriter writer = Files.newBufferedWriter(Path.of(output))) {
+
+            String line = reader1.readLine();
+
+            while (line != null) {
+                writer.write(line);
+                writer.newLine();
+                line = reader1.readLine();
+            }
+
+            line = reader2.readLine();
+
+            while (line != null) {
+                writer.write(line);
+                writer.newLine();
+                line = reader2.readLine();
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
