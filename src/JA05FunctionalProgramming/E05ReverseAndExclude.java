37d3a318ca049419de3e8e572a2bd47ee00d79c5
tproykov <tproykov@gmail.com>
2024-12-18 21:36:45 +0000

    update

--- src/JA05FunctionalProgramming/E05ReverseAndExclude.java
+++ src/JA05FunctionalProgramming/E05ReverseAndExclude.java
@@ -1,4 +1,31 @@
 package JA05FunctionalProgramming;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
 public class E05ReverseAndExclude {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        List<Integer> numbers = Arrays.stream(scanner.nextLine().split("\\s+"))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        Collections.reverse(numbers);
+
+        Predicate<Integer> isNotEven = number -> number % n != 0;
+
+        Consumer<Integer> printNumbers = number -> System.out.print(number + " ");
+
+        numbers.stream().filter(isNotEven).forEach(printNumbers);
+    }
 }
