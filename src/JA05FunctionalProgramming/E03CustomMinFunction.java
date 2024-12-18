d069df0d99a9d2c739f33c284deb50adb6764572
tproykov <tproykov@gmail.com>
2024-12-18 21:22:06 +0000

    update

--- src/JA05FunctionalProgramming/E03CustomMinFunction.java
+++ src/JA05FunctionalProgramming/E03CustomMinFunction.java
@@ -1,4 +1,31 @@
 package JA05FunctionalProgramming;
 
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.function.Function;
+
 public class E03CustomMinFunction {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        Function<Integer[], Integer> findSmallestNo = array -> {
+            int minNumber = Integer.MAX_VALUE;
+            for (Integer integer : array) {
+                if (integer < minNumber) {
+                    minNumber = integer;
+                }
+            }
+            return minNumber;
+        };
+
+        Integer[] numbers = Arrays.stream(scanner.nextLine().split(" "))
+                .map(Integer::parseInt)
+                .toArray(Integer[]::new);
+
+        Integer smallestNo = findSmallestNo.apply(numbers);
+
+        System.out.println(smallestNo);
+    }
 }
