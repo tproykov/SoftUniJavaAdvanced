f6545f78ba164691c67f8687a94cefca1eeee440
tproykov <tproykov@gmail.com>
2024-12-18 21:14:15 +0000

    update

--- src/JA05FunctionalProgramming/E02KnightsOfHonour.java
+++ src/JA05FunctionalProgramming/E02KnightsOfHonour.java
@@ -1,4 +1,18 @@
 package JA05FunctionalProgramming;
 
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.function.Consumer;
+
 public class E02KnightsOfHonour {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        Consumer<String> print = name -> System.out.println("Sir " + name);
+
+        Arrays.stream(scanner.nextLine().split("\\s+"))
+                .forEach(print);
+    }
 }
