05bb22a1098d71cbb1a8bebd79f775adf563700e
tproykov <tproykov@gmail.com>
2024-12-18 15:22:12 +0000

    update

--- src/JA05FunctionalProgramming/L04AddVat.java
+++ src/JA05FunctionalProgramming/L04AddVat.java
@@ -1,4 +1,20 @@
 package JA05FunctionalProgramming;
 
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
 public class L04AddVat {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Prices with VAT:");
+
+        Arrays.stream(scanner.nextLine().split(", "))
+                .mapToDouble(Double::parseDouble)
+                .map(price -> price * 1.20)
+                .forEach(price -> System.out.printf("%.2f\n", price));
+    }
 }
