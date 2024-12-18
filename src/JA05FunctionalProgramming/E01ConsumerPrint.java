d56aef254f7f575d3adc7fca3142f798a6232d0b
tproykov <tproykov@gmail.com>
2024-12-18 21:10:38 +0000

    update

--- src/JA05FunctionalProgramming/E01ConsumerPrint.java
+++ src/JA05FunctionalProgramming/E01ConsumerPrint.java
@@ -1,4 +1,18 @@
 package JA05FunctionalProgramming;
 
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.function.Consumer;
+
 public class E01ConsumerPrint {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        Consumer<String> print = System.out::println;
+
+        Arrays.stream(scanner.nextLine().split("\\s+"))
+                .forEach(print);
+    }
 }
