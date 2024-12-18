45ef6f01a70ea491db316256b452d632c0d505e3
tproykov <tproykov@gmail.com>
2024-12-18 15:12:09 +0000

    update

--- src/JA05FunctionalProgramming/L03CountUpperCaseWords.java
+++ src/JA05FunctionalProgramming/L03CountUpperCaseWords.java
@@ -1,4 +1,26 @@
 package JA05FunctionalProgramming;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class L03CountUpperCaseWords {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        String[] words = Arrays.stream(scanner.nextLine()
+                        .split("\\s+"))
+                .filter(word -> Character.isUpperCase(word.charAt(0)))
+                .toArray(String[]::new);
+
+        System.out.println(words.length);
+        for (String word : words) {
+            System.out.println(word);
+        }
+
+
+
+    }
+
 }
