e1b7bade149947d41da19fc64d59f55b8eecff37
tproykov <tproykov@gmail.com>
2024-12-19 22:39:12 +0000

    Update E06PredicateForNames.java

--- src/JA05FunctionalProgramming/E06PredicateForNames.java
+++ src/JA05FunctionalProgramming/E06PredicateForNames.java
@@ -1,4 +1,15 @@
 package JA05FunctionalProgramming;
 
+import java.util.Scanner;
+
 public class E06PredicateForNames {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        
+
+    }
+
 }
