49134693469288187e15b17a04fae6d40cb8b807
tproykov <tproykov@gmail.com>
2024-12-20 21:44:08 +0000

    update

--- src/JA05FunctionalProgramming/E07FindTheSmallestElement.java
+++ src/JA05FunctionalProgramming/E07FindTheSmallestElement.java
@@ -1,15 +1,31 @@
 package JA05FunctionalProgramming;
 
+import java.util.Arrays;
 import java.util.Scanner;
+import java.util.function.Function;
 
 public class E07FindTheSmallestElement {
-
     public static void main(String[] args) {
-
         Scanner scanner = new Scanner(System.in);
 
+        int[] numbers = Arrays.stream(scanner.nextLine().split("\\s+"))
+                .mapToInt(Integer::parseInt)
+                .toArray();
 
+        Function<int[], Integer> findLastIndexOfMin = array -> {
 
+            int minVal = array[array.length - 1];
+            int minIndex = array.length - 1;
+
+            for (int i = array.length - 2; i >= 0; i--) {
+                if (array[i] < minVal) {
+                    minVal = array[i];
+                    minIndex = i;
+                }
+            }
+            return minIndex;
+        };
+
+        System.out.println(findLastIndexOfMin.apply(numbers));
     }
-
 }
