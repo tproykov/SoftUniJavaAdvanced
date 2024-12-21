2c6ee8852911272f1c6105cb9f0d9f1948858037
tproykov <tproykov@gmail.com>
2024-12-21 14:19:32 +0000

    update

--- src/JA05FunctionalProgramming/E08CustomComparator.java
+++ src/JA05FunctionalProgramming/E08CustomComparator.java
@@ -1,5 +1,7 @@
 package JA05FunctionalProgramming;
 
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.Scanner;
 
 public class E08CustomComparator {
@@ -8,8 +10,21 @@ public class E08CustomComparator {
 
         Scanner scanner = new Scanner(System.in);
 
+        Comparator<Integer> customComparator = (first, second) -> {
 
+            if (first % 2 == 0 && second % 2 != 0) {
+                return -101;
+            } else if (first % 2 != 0 && second % 2 == 0) {
+                return 101;
+            } else {
+                return first.compareTo(second);
+            }
 
+        };
+
+        Arrays.stream(scanner.nextLine().split("\\s+"))
+                .map(Integer::parseInt)
+                .sorted(customComparator)
+                .forEach(n -> System.out.print(n + " "));
     }
-
 }
