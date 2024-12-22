7dc5efd2d9544c018deea7b79336a3273ec1fb51
tproykov <tproykov@gmail.com>
2024-12-22 23:08:09 +0000

    update

--- src/JA06DefiningClasses/L01CarInfo/Main.java
+++ src/JA06DefiningClasses/L01CarInfo/Main.java
@@ -8,8 +8,12 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
 
-        
+        Car car = new Car();
 
+        car.setBrand("Chevrolet");
+        car.setModel("Impala");
+        car.setHorsePower(390);
+
+        System.out.printf("The car is: %s %s - %d HP", car.getBrand(), car.getModel(), car.getHorsePower());
     }
-
 }
