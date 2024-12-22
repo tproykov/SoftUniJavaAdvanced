c678acb3a285d6c5a2193dd61d437b42c2f6e0e0
tproykov <tproykov@gmail.com>
2024-12-22 21:42:26 +0000

    udate

--- src/JA05FunctionalProgramming/E09ListOfPredicates.java
+++ src/JA05FunctionalProgramming/E09ListOfPredicates.java
@@ -1,6 +1,10 @@
 package JA05FunctionalProgramming;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
+import java.util.function.Predicate;
+import java.util.stream.IntStream;
 
 public class E09ListOfPredicates {
 
@@ -8,8 +12,22 @@ public class E09ListOfPredicates {
 
         Scanner scanner = new Scanner(System.in);
 
+        int n = Integer.parseInt(scanner.nextLine());
 
+        List<Integer> sequence = Arrays.stream(scanner.nextLine().split("\\s+"))
+                .map(Integer::parseInt)
+                .toList();
 
+        Predicate<Integer> isDivisible = num -> sequence.stream()
+                .filter(x -> x != 0)  // Avoid division by zero
+                .allMatch(x -> num % x == 0);
+
+        String result = IntStream.rangeClosed(1, n)
+                .filter(num -> isDivisible.test(num))
+                .mapToObj(String::valueOf)
+                .reduce((a, b) -> a + " " + b)
+                .orElse("");
+
+        System.out.println(result);
     }
-
 }
