4e591f67aeec31c571e2be1528ea2ee7880b2052
tproykov <tproykov@gmail.com>
2024-12-22 23:29:25 +0000

    Update Car.java

--- src/JA06DefiningClasses/L02CarConstructors/Car.java
+++ src/JA06DefiningClasses/L02CarConstructors/Car.java
@@ -6,6 +6,17 @@ public class Car {
     private String model;
     private int horsePower;
 
+    public Car(String brand) {
+        this.brand = brand;
+        this.model = "unknown";
+        this.horsePower = -1;
+    }
+
+    public Car(String brand, String model, int horsePower) {
+        this.brand = brand;
+        this.model = model;
+        this.horsePower = horsePower;
+    }
 
     public String getBrand() {
         return brand;
