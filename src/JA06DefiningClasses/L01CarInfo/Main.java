175a614b7b8dc3ec33abb67d26091bb70928d145
tproykov <tproykov@gmail.com>
2024-12-22 22:48:32 +0000

    Create Main.java

---
+++ src/JA06DefiningClasses/L01CarInfo/Main.java
@@ -0,0 +1,15 @@
+package JA06DefiningClasses.L01CarInfo;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        
+
+    }
+
+}
