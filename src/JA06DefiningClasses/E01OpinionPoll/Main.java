b3b5431f1d080fc9a55a3278d612425b23303010
tproykov <tproykov@gmail.com>
2024-12-23 09:22:05 +0000

    update

--- src/JA06DefiningClasses/E01OpinionPoll/Main.java
+++ src/JA06DefiningClasses/E01OpinionPoll/Main.java
@@ -1,4 +1,31 @@
 package JA06DefiningClasses.E01OpinionPoll;
 
+import java.util.*;
+
 public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        List<Person> people = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+
+            String[] tokens = scanner.nextLine().split("\\s+");
+            String name = tokens[0];
+            int age = Integer.parseInt(tokens[1]);
+
+            Person person = new Person(name, age);
+            people.add(person);
+        }
+
+        people.stream()
+                .filter(person -> person.getAge() > 30)
+                .sorted((person1, person2) -> person2.getName().compareTo(person1.getName()))
+                .sorted(Comparator.comparing(Person::getName))
+                .forEach(System.out::println);
+    }
 }
