2f9e22f580262697bbfda58e8e38cfaadd5b2d78
tproykov <tproykov@gmail.com>
2024-12-23 00:19:06 +0000

    update

--- src/JA06DefiningClasses/L03BankAccount/Main.java
+++ src/JA06DefiningClasses/L03BankAccount/Main.java
@@ -1,5 +1,7 @@
 package JA06DefiningClasses.L03BankAccount;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
@@ -8,8 +10,47 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
 
-        
+        Map<Integer, BankAccount> accounts = new HashMap<>();
 
+        String command;
+        while (!"End".equals(command = scanner.nextLine())) {
+
+            String[] tokens = command.split("\\s+");
+            String commandType = tokens[0];
+
+            switch (commandType) {
+                case "Create" -> {
+                    BankAccount account = new BankAccount();
+                    accounts.put(account.getId(), account);
+                    System.out.println("Account ID" + account.getId() + " created");
+                }
+
+                case "Deposit" -> {
+                    int id = Integer.parseInt(tokens[1]);
+                    double amount = Double.parseDouble(tokens[2]);
+                    if (accounts.containsKey(id)) {
+                        accounts.get(id).deposit(amount);
+                        System.out.printf("Deposited %.0f to ID%d\n", amount, id);
+                    } else {
+                        System.out.println("Account does not exist");
+                    }
+                }
+
+                case "SetInterest" -> {
+                    double interest = Double.parseDouble(tokens[1]);
+                    BankAccount.setInterestRate(interest);
+                }
+
+                case "GetInterest" -> {
+                    int id = Integer.parseInt(tokens[1]);
+                    int years = Integer.parseInt(tokens[2]);
+                    if (accounts.containsKey(id)) {
+                        System.out.printf("%.2f\n", accounts.get(id).getInterest(years));
+                    } else {
+                        System.out.println("Account does not exist");
+                    }
+                }
+            }
+        }
     }
-
 }
