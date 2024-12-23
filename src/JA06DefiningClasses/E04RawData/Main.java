e2ac2a3f461bc7ce4d90f5a9e3edbccbb605aa78
tproykov <tproykov@gmail.com>
2024-12-23 17:17:44 +0000

    Update Main.java

--- src/JA06DefiningClasses/E04RawData/Main.java
+++ src/JA06DefiningClasses/E04RawData/Main.java
@@ -6,8 +6,11 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
+
         int n = Integer.parseInt(scanner.nextLine());
+
         List<Car> cars = new ArrayList<>();
 
         for (int i = 0; i < n; i++) {
