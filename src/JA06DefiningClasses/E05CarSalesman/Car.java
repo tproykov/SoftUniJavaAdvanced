a5649f722035d614fccf611ca686fffd4fbe4bbb
tproykov <tproykov@gmail.com>
2024-12-24 21:10:39 +0000

    update

--- src/JA06DefiningClasses/E05CarSalesman/Car.java
+++ src/JA06DefiningClasses/E05CarSalesman/Car.java
@@ -1,4 +1,17 @@
 package JA06DefiningClasses.E05CarSalesman;
 
 public class Car {
+
+    private String model;
+    private Engine engine;
+    private int weight;
+    private String color;
+
+    public Car(String model, Engine engine) {
+        this.model = model;
+        this.engine = engine;
+        this.weight = 0;
+        this.color = "";
+
+    }
 }
--- src/JA06DefiningClasses/E05CarSalesman/Engine.java
+++ src/JA06DefiningClasses/E05CarSalesman/Engine.java
@@ -1,4 +1,21 @@
 package JA06DefiningClasses.E05CarSalesman;
 
 public class Engine {
+
+    private String model;
+    private int horsePower;
+    private int displacement;
+    private int efficiency;
+
+    public Engine(String model, int horsePower) {
+        this.model = model;
+        this.horsePower = horsePower;
+        this.displacement = 0;
+        this.efficiency = 0;
+
+
+
+        
+
+    }
 }
