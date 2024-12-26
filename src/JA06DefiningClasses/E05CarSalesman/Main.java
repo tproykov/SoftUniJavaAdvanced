2ea97d9b37ecd1bd75499c50a547dbbc50d9b9c0
tproykov <tproykov@gmail.com>
2024-12-26 21:36:47 +0000

    Update Main.java

--- src/JA06DefiningClasses/E05CarSalesman/Main.java
+++ src/JA06DefiningClasses/E05CarSalesman/Main.java
@@ -51,31 +51,41 @@ public class Main {
 
         List<Car> cars = new ArrayList<>();
         for (int i = 0; i < m; i++) {
-
             String[] tokens = scanner.nextLine().split("\\s+");
             String model = tokens[0];
-            String engine = tokens[1];
+            String engineModel = tokens[1];
+
+            // Find the engine object with the given model
+            Engine engineObject = null;
+            for (Engine eng : engines) {
+                if (eng.getModel().equals(engineModel)) {
+                    engineObject = eng;
+                    break;
+                }
+            }
 
             Car car;
             if (tokens.length == 4) {
                 int weight = Integer.parseInt(tokens[2]);
                 String color = tokens[3];
-
-                car = new Car(model, Engine, weight, color)
+                car = new Car(model, engineObject, weight, color);
+            } else if (tokens.length == 3) {
+                try {
+                    int weight = Integer.parseInt(tokens[2]);
+                    car = new Car(model, engineObject, weight);
+                } catch (NumberFormatException e) {
+                    String color = tokens[2];
+                    car = new Car(model, engineObject, color);
+                }
+            } else {
+                car = new Car(model, engineObject);
             }
+            cars.add(car);
+        }
 
-
-
-
-
-
-
-
-
-
-
-
-
+// Print all cars
+        for (Car car : cars) {
+            System.out.println(car);
         }
 
 
