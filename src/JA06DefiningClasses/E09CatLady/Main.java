30b10ec30c184c3eab37755721e7f1ce71b9c583
tproykov <tproykov@gmail.com>
2024-12-27 23:03:44 +0000

    update

--- src/JA06DefiningClasses/E09CatLady/Main.java
+++ src/JA06DefiningClasses/E09CatLady/Main.java
@@ -1,4 +1,15 @@
 package JA06DefiningClasses.E09CatLady;
 
+import java.util.Scanner;
+
 public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+
+        
+    }
+
 }
