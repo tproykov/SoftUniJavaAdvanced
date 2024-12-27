132bf97b4b4dedcce84dfd6b71930c0c28f53cf9
tproykov <tproykov@gmail.com>
2024-12-27 23:03:18 +0000

    update

--- src/JA06DefiningClasses/E07Google/Main.java
+++ src/JA06DefiningClasses/E07Google/Main.java
@@ -1,4 +1,15 @@
 package JA06DefiningClasses.E07Google;
 
+import java.util.Scanner;
+
 public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+
+
+    }
+
 }
