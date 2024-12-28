02c26c0d4b25b5a91da5a20158af66f732d5824e
tproykov <tproykov@gmail.com>
2024-12-28 21:27:39 +0000

    Create E093MouseInTheKitchen.java

---
+++ src/JA11ExamPreparation/E093MouseInTheKitchen.java
@@ -0,0 +1,15 @@
+package JA11ExamPreparation;
+
+import java.util.Scanner;
+
+public class E093MouseInTheKitchen {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        
+
+    }
+
+}
