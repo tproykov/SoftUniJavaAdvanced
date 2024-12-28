27e10794a7235996cf0fe55fba537869622223d9
tproykov <tproykov@gmail.com>
2024-12-28 19:22:03 +0000

    update

---
+++ src/JA07Workshop/L01SmartArray/Main.java
@@ -0,0 +1,22 @@
+package JA07Workshop.L01SmartArray;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(5);
+        list.add(10);
+        list.add(15);
+        list.add(20);
+
+        SmartArray smartArray = new SmartArray();
+
+    }
+
+}
---
+++ src/JA07Workshop/L01SmartArray/SmartArray.java
@@ -0,0 +1,17 @@
+package JA07Workshop.L01SmartArray;
+
+public class SmartArray {
+
+    private int[] data;
+    private int size;
+    private int capacity;
+
+    public SmartArray() {
+        this.size = 0;
+        this.capacity = 4;
+        this.data = new int[capacity];
+
+
+    }
+
+}
