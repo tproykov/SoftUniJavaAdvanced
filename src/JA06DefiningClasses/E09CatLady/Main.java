e7ddf98751edc991d7aa3c6122e78c9eb2d0f906
tproykov <tproykov@gmail.com>
2024-12-28 11:39:33 +0000

    update

--- src/JA06DefiningClasses/E09CatLady/Main.java
+++ src/JA06DefiningClasses/E09CatLady/Main.java
@@ -1,5 +1,7 @@
 package JA06DefiningClasses.E09CatLady;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
@@ -8,8 +10,35 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
 
+        Map<String, Cat> cats = new HashMap<>();
 
+        String input;
+        while (!(input = scanner.nextLine()).equals("End")) {
+            String[] tokens = input.split("\\s+");
+            String breed = tokens[0];
+            String name = tokens[1];
+            double value = Double.parseDouble(tokens[2]);
 
+            Cat cat;
+            switch (breed) {
+                case "Siamese":
+                    cat = new Siamese(name, value);
+                    break;
+                case "Cymric":
+                    cat = new Cymric(name, value);
+                    break;
+                case "StreetExtraordinaire":
+                    cat = new StreetExtraordinaire(name, value);
+                    break;
+                default:
+                    continue;
+            }
+            cats.put(name, cat);
+        }
+
+        String catName = scanner.nextLine();
+        if (cats.containsKey(catName)) {
+            System.out.println(cats.get(catName));
+        }
     }
-
 }
---
+++ src/JA06DefiningClasses/E09CatLady/Siamese.java
@@ -0,0 +1,16 @@
+package JA06DefiningClasses.E09CatLady;
+
+public class Siamese extends Cat {
+
+    private double earSize;
+
+    public Siamese(String name, double earSize) {
+        super(name);
+        this.earSize = earSize;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Siamese %s %.2f", getName(), earSize);
+    }
+}
