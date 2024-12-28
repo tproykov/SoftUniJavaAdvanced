f7551ee80c21fc6018d984d3799f3cc6069c432b
tproykov <tproykov@gmail.com>
2024-12-28 03:40:59 +0000

    update all

--- src/JA06DefiningClasses/E06PokemonTrainer/Main.java
+++ src/JA06DefiningClasses/E06PokemonTrainer/Main.java
@@ -1,21 +1,15 @@
 package JA06DefiningClasses.E06PokemonTrainer;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
-
     public static void main(String[] args) {
-
         Scanner scanner = new Scanner(System.in);
 
-        List<Trainer> trainers = new ArrayList<>();
-        List<Pokemon> pokemons = new ArrayList<>();
+        Map<String, Trainer> trainers = new LinkedHashMap<>();
 
         String command1;
         while (!"Tournament".equals(command1 = scanner.nextLine())) {
-
             String[] tokens = command1.split("\\s+");
 
             String trainerName = tokens[0];
@@ -24,20 +18,29 @@ public class Main {
             int pokemonHealth = Integer.parseInt(tokens[3]);
 
             Pokemon pokemon = new Pokemon(pokemonName, pokemonElement, pokemonHealth);
-            pokemons.add(pokemon);
 
-
-
-
-
-
-
-
-
-
+            trainers.putIfAbsent(trainerName, new Trainer(trainerName));
+            trainers.get(trainerName).addPokemon(pokemon);
         }
 
+        String command2;
+        while (!"End".equals(command2 = scanner.nextLine())) {
+            String element = command2;
 
+            for (Trainer trainer : trainers.values()) {
+                if (trainer.hasElementType(element)) {
+                    trainer.increaseBadges();
+                } else {
+                    trainer.reduceHealth();
+                }
+            }
+        }
 
+        trainers.values().stream()
+                .sorted((t1, t2) -> Integer.compare(t2.getNumberOfBadges(), t1.getNumberOfBadges()))
+                .forEach(trainer -> System.out.printf("%s %d %d%n",
+                        trainer.getName(),
+                        trainer.getNumberOfBadges(),
+                        trainer.getCollectionOfPokemons().size()));
     }
 }
--- src/JA06DefiningClasses/E06PokemonTrainer/Trainer.java
+++ src/JA06DefiningClasses/E06PokemonTrainer/Trainer.java
@@ -1,16 +1,46 @@
 package JA06DefiningClasses.E06PokemonTrainer;
 
 import java.util.List;
+import java.util.ArrayList;
 
 public class Trainer {
-
     private String name;
     private int numberOfBadges;
     private List<Pokemon> collectionOfPokemons;
 
-    public Trainer(String name, int numberOfBadges, List<Pokemon> collectionOfPokemons) {
+    public Trainer(String name) {
         this.name = name;
-        this.numberOfBadges = numberOfBadges;
-        this.collectionOfPokemons = collectionOfPokemons;
+        this.numberOfBadges = 0;
+        this.collectionOfPokemons = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getNumberOfBadges() {
+        return numberOfBadges;
+    }
+
+    public List<Pokemon> getCollectionOfPokemons() {
+        return collectionOfPokemons;
+    }
+
+    public void addPokemon(Pokemon pokemon) {
+        collectionOfPokemons.add(pokemon);
+    }
+
+    public void increaseBadges() {
+        numberOfBadges++;
+    }
+
+    public boolean hasElementType(String element) {
+        return collectionOfPokemons.stream()
+                .anyMatch(pokemon -> pokemon.getElement().equals(element));
+    }
+
+    public void reduceHealth() {
+        collectionOfPokemons.forEach(pokemon -> pokemon.setHealth(pokemon.getHealth() - 10));
+        collectionOfPokemons.removeIf(pokemon -> pokemon.getHealth() <= 0);
     }
 }
