eb54e9cc16a2d8627fd1051133a05e22df92599d
tproykov <tproykov@gmail.com>
2024-12-29 12:34:44 +0000

    update

--- src/JA08Generics/L02GenericArrayCreator/ArrayCreator.java
+++ src/JA08Generics/L02GenericArrayCreator/ArrayCreator.java
@@ -1,10 +1,27 @@
 package JA08Generics.L02GenericArrayCreator;
 
-public class ArrayCreator <T> {
+import java.lang.reflect.Array;
 
+public class ArrayCreator {
 
+    // {access_modifier} static {<generic_parameter>} {return_type} {name}(){}
+    public static <T> T[] create(int length, T item) {
 
+        T[] array = (T[]) Array.newInstance(item.getClass(), length);
 
+        for (int i = 0; i < length; i++) {
+            array[i] = item;
+        }
+        return array;
+    }
 
+    public static  <T> T[] create(Class<T> cl, int length, T item) {
 
+        T[] array = (T[]) Array.newInstance(cl, length);
+
+        for (int i = 0; i < length; i++) {
+            array[i] = item;
+        }
+        return array;
+    }
 }
--- src/JA08Generics/L02GenericArrayCreator/Main.java
+++ src/JA08Generics/L02GenericArrayCreator/Main.java
@@ -1,4 +1,15 @@
 package JA08Generics.L02GenericArrayCreator;
 
+import java.util.Arrays;
+
 public class Main {
+
+    public static void main(String[] args) {
+
+        String[] names = ArrayCreator.create(5, "Hello");
+        Integer[] numbers = ArrayCreator.create(5, 3);
+
+        System.out.println(Arrays.toString(names));
+        System.out.println(Arrays.toString(numbers));
+    }
 }
