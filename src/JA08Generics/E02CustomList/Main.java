2c73b0613b68dcf1addc2a0f1aed8a6416c911d6
tproykov <tproykov@gmail.com>
2024-12-29 19:31:55 +0000

    update

--- src/JA08Generics/E02CustomList/Main.java
+++ src/JA08Generics/E02CustomList/Main.java
@@ -10,47 +10,47 @@ public class Main {
 
         CustomList<String> list = new CustomList<>();
 
-        String input = scanner.nextLine();
-        while (!input.equals("END")) {
+        String command;
+        while (!("END".equals(command = scanner.nextLine()))) {
 
-            String command = input.split("\\s+")[0];
-            switch (command) {
-                case "Add":
-                    String elementToAdd = input.split("\\s+")[1];
+            String[] tokens = command.split("\\s+");
+            String commandType = tokens[0];
+
+            switch (commandType) {
+                case "Add" -> {
+                    String elementToAdd = tokens[1];
                     list.add(elementToAdd);
-                    break;
-                case "Remove":
-                    int index = Integer.parseInt(input.split("\\s+")[1]);
-                    list.remove(index);
-                    break;
-                case "Contains":
-                    String elementToCheck = input.split("\\s+")[1];
-                    System.out.println(list.contains(elementToCheck));
-                    break;
-                case "Swap":
-                    int index1 = Integer.parseInt(input.split("\\s+")[1]);
-                    int index2 = Integer.parseInt(input.split("\\s+")[2]);
-                    list.swap(index1, index2);
-                    break;
-                case "Greater":
-                    String elementToCompareWith = input.split("\\s+")[1];
-                    System.out.println(list.countGreaterValues(elementToCompareWith));
-                    break;
-                case "Max":
-                    System.out.println(list.getMax());
-                    break;
-                case "Min":
-                    System.out.println(list.getMin());
-                    break;
-                case "Print":
-                    System.out.println(list);
-                    break;
-                case "Sort":
-                    list.sort();
-                    break;
-            }
+                }
 
-            input = scanner.nextLine();
+                case "Remove" -> {
+                    int index = Integer.parseInt(tokens[1]);
+                    list.remove(index);
+                }
+
+                case "Contains" -> {
+                    String elementToCheck = tokens[1];
+                    System.out.println(list.contains(elementToCheck));
+                }
+
+                case "Swap" -> {
+                    int index1 = Integer.parseInt(tokens[1]);
+                    int index2 = Integer.parseInt(tokens[2]);
+                    list.swap(index1, index2);
+                }
+
+                case "Greater" -> {
+                    String elementToCompareWith = tokens[1];
+                    System.out.println(list.countGreaterThan(elementToCompareWith));
+                }
+
+                case "Max" -> System.out.println(list.getMax());
+
+                case "Min" -> System.out.println(list.getMin());
+
+                case "Print" -> System.out.println(list);
+
+                case "Sort" -> list.sort();
+            }
         }
     }
 }
