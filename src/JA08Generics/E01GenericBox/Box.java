992f9263b848bd1b81827fb475b5b0ad01822870
tproykov <tproykov@gmail.com>
2024-12-29 18:09:25 +0000

    update

--- src/JA08Generics/E01GenericBox/Box.java
+++ src/JA08Generics/E01GenericBox/Box.java
@@ -1,20 +1,53 @@
 package JA08Generics.E01GenericBox;
 
-public class Box<T extends Comparable<T>> implements Comparable<Box<T>> {
+import java.util.ArrayList;
+import java.util.List;
 
-    public T element;
+public class Box<T extends Comparable<T>> {
 
-    public Box(T element) {
-        this.element = element;
+    private List<T> values;
+
+    public Box() {
+        this.values = new ArrayList<>();
+    }
+
+    public void add(T value) {
+        this.values.add(value);
+    }
+
+    public void swap(int index1, int index2) {
+
+        // Collections.swap(this.values, index1, index2);
+        T firstValue = values.get(index1);
+        T secondValue = values.get(index2);
+
+        this.values.set(index1, secondValue);
+        this.values.set(index2, firstValue);
+    }
+
+    public int countGreaterValues(T valueToCompare) {
+
+        int count = 0;
+        for (T value : this.values) {
+
+            int comparingResult = value.compareTo(valueToCompare);
+            if (comparingResult > 0) {
+                count++;
+            }
+        }
+        return count;
     }
 
     @Override
     public String toString() {
-        return String.format("%s: %s", this.element.getClass().getName(), this.element);
-    }
 
-    @Override
-    public int compareTo(Box<T> o) {
-        return this.element.compareTo(o.element);
+        StringBuilder sb = new StringBuilder();
+        for (T value : this.values) {
+            //"{class full name}: {value}"
+            String formattedValue = String.format("%s: %s", value.getClass().getName(), value);
+            sb.append(formattedValue).append(System.lineSeparator());
+        }
+
+        return sb.toString();
     }
 }
--- src/JA08Generics/E01GenericBox/Main.java
+++ src/JA08Generics/E01GenericBox/Main.java
@@ -10,10 +10,53 @@ public class Main {
 
         int n = Integer.parseInt(scanner.nextLine());
 
-        for (int i = 0; i < n; i++) {
-            String line = scanner.nextLine();
-            Box<String> box = new Box<>(line);
-            System.out.println(box);
-        }
+        // Exercise 1:
+//        Box<String> box = new Box<>();
+//        for (int row = 0; row < n; row++) {
+//            String line = scanner.nextLine();
+//            box.add(line);
+//        }
+
+        // Exercise 2:
+//        Box<Integer> box = new Box<>();
+//        for (int row = 0; row < n; row++) {
+//            int number = Integer.parseInt(scanner.nextLine());
+//            box.add(number);
+//        }
+
+        // Exercise 3:
+//        Box<String> box = new Box<>();
+//        for (int row = 0; row < n; row++) {
+//            String line = scanner.nextLine();
+//            box.add(line);
+//        }
+
+        // Exercise 4:
+//        Box<Integer> box = new Box<>();
+//        for (int row = 0; row < n; row++) {
+//            int number = Integer.parseInt(scanner.nextLine());
+//            box.add(number);
+//        }
+//        int index1 = scanner.nextInt();
+//        int index2 = scanner.nextInt();
+//        box.swap(index1, index2);
+
+        // Exercise 5:
+//        Box<String> box = new Box<>();
+//        for (int row = 0; row < n; row++) {
+//            String line = scanner.nextLine();
+//            box.add(line);
+//        }
+//        String valueToCompare = scanner.nextLine();
+
+        // Exercise 6:
+//        Box<Double> box = new Box<>();
+//        for (int row = 0; row < n; row++) {
+//            double number = Double.parseDouble(scanner.nextLine());
+//            box.add(number);
+//        }
+//        double valueToCompare = Double.parseDouble(scanner.nextLine());
+//
+//        System.out.println(box.countGreaterValues(valueToCompare));
     }
 }
