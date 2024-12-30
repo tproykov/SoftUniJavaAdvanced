febae8c14ea015da467906b56b487cbd2f0b8c29
tproykov <tproykov@gmail.com>
2024-12-30 12:23:46 +0000

    update

---
+++ src/JA09IteratorsAndComparators/L01Book/Book.java
@@ -0,0 +1,44 @@
+package JA09IteratorsAndComparators.L01Book;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Book {
+
+    private String title;
+    private int year;
+    private List<String> authors;
+
+    public Book(String title, int year, String... authors) {
+        setTitle(title);
+        setYear(year);
+        setAuthors(authors);
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    private void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    private void setYear(int year) {
+        this.year = year;
+    }
+
+    public List<String> getAuthors() {
+        return authors;
+    }
+
+    private void setAuthors(String... authors) {
+        this.authors = new ArrayList<>();
+        for (String author : authors) {
+            this.authors.add(author);
+        }
+    }
+}
