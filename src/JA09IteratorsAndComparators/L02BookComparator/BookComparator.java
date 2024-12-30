8c2a7b5709839d8e5df78e4f6591299093f3276b
tproykov <tproykov@gmail.com>
2024-12-30 16:51:55 +0000

    update

--- src/JA09IteratorsAndComparators/L02BookComparator/BookComparator.java
+++ src/JA09IteratorsAndComparators/L02BookComparator/BookComparator.java
@@ -1,4 +1,20 @@
 package JA09IteratorsAndComparators.L02BookComparator;
 
-public class BookComparator {
+import java.util.Comparator;
+
+public class BookComparator implements Comparator<Book> {
+
+    @Override
+    public int compare(Book book1, Book book2) {
+
+        int result = book1.getTitle().compareTo(book2.getTitle());
+        if (result == 0) {
+            result = Integer.compare(book1.getYear(), book2.getYear());
+        }
+        return result;
+    }
+
+
+
+
 }
