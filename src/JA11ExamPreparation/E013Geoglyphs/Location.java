ba6d74a50915b7d454913a40b82e57e9c11d82f9
tproykov <tproykov@gmail.com>
2025-01-01 20:52:24 +0000

    Update Location.java

--- src/JA11ExamPreparation/E013Geoglyphs/Location.java
+++ src/JA11ExamPreparation/E013Geoglyphs/Location.java
@@ -1,40 +1,84 @@
 package JA11ExamPreparation.E013Geoglyphs;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.stream.Collectors;
 
 public class Location {
-
     private String name;
-    private Set<Geoglyph> geoglyphs;
+    private Collection<Geoglyph> geoglyphs;
 
     public Location(String name) {
         this.name = name;
-        this.geoglyphs = new HashSet<>();
+        this.geoglyphs = new ArrayList<>();
     }
 
-    public void addGeoglyph(Geoglyph geoglyph) {
-        if (geoglyphs.contains(geoglyph)) {
+    public String addGeoglyph(Geoglyph geoglyph) {
+        if (this.geoglyphs.stream().anyMatch(g -> g.getName().equals(geoglyph.getName()))) {
             throw new IllegalArgumentException("This geoglyph exists in the location!");
         }
-        else {
-            this.geoglyphs.add(geoglyph);
-            System.out.println(geoglyph + " added to " + this.name);
-        }
+        this.geoglyphs.add(geoglyph);
+        return String.format("%s added to %s.", geoglyph.getName(), this.name);
     }
 
-    public boolean removeGeoglyph(Geoglyph geoglyph) {
-        if (!geoglyphs.contains(geoglyph)) {
-            return false;
-        }
-        else {
-            this.geoglyphs.remove(geoglyph);
-            return true;
-        }
+    public boolean removeGeoglyph(String name) {
+        return this.geoglyphs.removeIf(g -> g.getName().equals(name));
     }
 
     public String getGeoglyphsByConstructionTechnique(String constructionTechnique) {
-        return 
+        Collection<String> matchingGeoglyphs = this.geoglyphs.stream()
+                .filter(g -> g.getConstructionTechnique().equals(constructionTechnique))
+                .map(Geoglyph::getName)
+                .collect(Collectors.toList());
+
+        if (matchingGeoglyphs.isEmpty()) {
+            return String.format("There are no geoglyphs made by %s in this location.", constructionTechnique);
+        }
+
+        return String.format("Geoglyphs made by %s: %s",
+                constructionTechnique,
+                String.join(", ", matchingGeoglyphs));
     }
 
+    public String getLargestGeoglyph() {
+        if (this.geoglyphs.isEmpty()) {
+            return String.format("No geoglyphs have yet been discovered in %s.", this.name);
+        }
+
+        Geoglyph largest = this.geoglyphs.stream()
+                .max((g1, g2) -> Integer.compare(g1.getSize(), g2.getSize()))
+                .orElse(null);
+
+        return String.format("The largest geoglyph is %s with a length of %d meters.",
+                largest.getName(),
+                largest.getSize());
+    }
+
+    public Geoglyph getGeoglyphByName(String name) {
+        return this.geoglyphs.stream()
+                .filter(g -> g.getName().equals(name))
+                .findFirst()
+                .orElse(null);
+    }
+
+    public int getCount() {
+        return this.geoglyphs.size();
+    }
+
+    public String getStatistics() {
+        if (this.geoglyphs.isEmpty()) {
+            return String.format("No geoglyphs have yet been discovered in %s.", this.name);
+        }
+
+        StringBuilder statistics = new StringBuilder()
+                .append(String.format("Geoglyphs located in %s:", this.name))
+                .append(System.lineSeparator());
+
+        this.geoglyphs.forEach(g -> statistics
+                .append("* ")
+                .append(g.getName())
+                .append(System.lineSeparator()));
+
+        return statistics.toString().trim();
+    }
 }
