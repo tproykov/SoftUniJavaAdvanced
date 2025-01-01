3ebdcc885d5350e84fb88bb466981d145045b095
tproykov <tproykov@gmail.com>
2025-01-01 13:11:35 +0000

    Update Location.java

--- src/JA11ExamPreparation/E013Geoglyphs/Location.java
+++ src/JA11ExamPreparation/E013Geoglyphs/Location.java
@@ -13,6 +13,28 @@ public class Location {
         this.geoglyphs = new HashSet<>();
     }
 
-    
+    public void addGeoglyph(Geoglyph geoglyph) {
+        if (geoglyphs.contains(geoglyph)) {
+            throw new IllegalArgumentException("This geoglyph exists in the location!");
+        }
+        else {
+            this.geoglyphs.add(geoglyph);
+            System.out.println(geoglyph + " added to " + this.name);
+        }
+    }
+
+    public boolean removeGeoglyph(Geoglyph geoglyph) {
+        if (!geoglyphs.contains(geoglyph)) {
+            return false;
+        }
+        else {
+            this.geoglyphs.remove(geoglyph);
+            return true;
+        }
+    }
+
+    public String getGeoglyphsByConstructionTechnique(String constructionTechnique) {
+        return 
+    }
 
 }
