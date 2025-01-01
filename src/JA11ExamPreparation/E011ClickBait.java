550ed2d38baba15f70c1d9303752b92b052fb236
tproykov <tproykov@gmail.com>
2025-01-01 09:38:46 +0000

    update

---
+++ src/JA11ExamPreparation/E011ClickBait.java
@@ -0,0 +1,21 @@
+package JA11ExamPreparation;
+
+import java.util.Scanner;
+
+public class E011ClickBait {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        
+
+    }
+
+
+
+
+
+
+
+}
