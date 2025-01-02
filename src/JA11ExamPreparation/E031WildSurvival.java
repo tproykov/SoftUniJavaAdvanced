7c11b6c1d8235ddce1e648c8c30e6102627d4f42
tproykov <tproykov@gmail.com>
2025-01-02 19:34:15 +0000

    Update E031WildSurvival.java

--- src/JA11ExamPreparation/E031WildSurvival.java
+++ src/JA11ExamPreparation/E031WildSurvival.java
@@ -1,6 +1,7 @@
 package JA11ExamPreparation;
 
-import java.util.Scanner;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class E031WildSurvival {
 
@@ -8,7 +9,35 @@ public class E031WildSurvival {
 
         Scanner scanner = new Scanner(System.in);
 
-        
+        Queue<Integer> beeGroups = Arrays.stream(scanner.nextLine()
+                .split(" "))
+                .map(Integer::parseInt)
+                .collect(Collectors.toCollection(LinkedList::new));
+
+        Stack<Integer> beeEaterGroups = Arrays.stream(scanner.nextLine()
+                .split(" "))
+                .map(Integer::parseInt)
+                .collect(Collectors.toCollection(Stack::new));
+
+        while (!beeGroups.isEmpty() && !beeEaterGroups.isEmpty()) {
+
+            int firstBeeGroup = beeGroups.peek();
+            int lastBeeEaterGroup = beeEaterGroups.peek();
+
+            if ( lastBeeEaterGroup > firstBeeGroup * 7) {
+
+
+
+
+            }
+
+
+        }
+
+
+
+
+
 
     }
 
