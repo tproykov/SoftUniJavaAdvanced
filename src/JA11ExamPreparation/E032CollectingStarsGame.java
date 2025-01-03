bcc5f6b5df94372af2aab34b7ace44cfd45d6de6
tproykov <tproykov@gmail.com>
2025-01-03 03:10:03 +0000

    Update E032CollectingStarsGame.java

--- src/JA11ExamPreparation/E032CollectingStarsGame.java
+++ src/JA11ExamPreparation/E032CollectingStarsGame.java
@@ -1,12 +1,36 @@
 package JA11ExamPreparation;
 
+import java.util.Arrays;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class E032CollectingStarsGame {
 
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
+
+        int fieldSize = Integer.parseInt(scanner.nextLine());
+
+        char[][] field = new char[fieldSize][fieldSize];
+        int playerRowPosition = 0;
+        int playerColPosition = 0;
+
+        for (int row = 0; row < fieldSize; row++) {
+            char[] line = scanner.nextLine().replaceAll(" ", "").toCharArray();
+
+            for (int col = 0; col < fieldSize; col++) {
+                field[row][col] = line[col];
+                if (field[row][col] == 'P') {
+                    playerRowPosition = row;
+                    playerColPosition = col;
+                }
+            }
+        }
+
+
+
+
 
 
 
