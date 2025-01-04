415c1f4bc83688fdf94704763fe590738c0680a9
tproykov <tproykov@gmail.com>
2025-01-04 03:00:45 +0000

    Update Beach.java

--- src/JA11ExamPreparation/E033Surfers/Beach.java
+++ src/JA11ExamPreparation/E033Surfers/Beach.java
@@ -1,6 +1,7 @@
 package JA11ExamPreparation.E033Surfers;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class Beach {
@@ -19,25 +20,75 @@ public class Beach {
 
         if (!surfer.ownsASurfBoard() && surfer.getMoney() < 50) {
             System.out.println(surfer.getName() + " has not enough money to rent a surfboard.");
-        }
-        else if (surfboardsForRent == 0) {
+        } else if (surfboardsForRent == 0) {
             System.out.println("There are no free surfboards.");
-        }
-        else {
+        } else {
             surfers.add(surfer);
             surfboardsForRent--;
         }
     }
 
     public boolean removeSurfer(String name) {
-        if (surfers.contains(name)) {
-            surfers.remove(name);
-            return true;
+        return surfers.removeIf(surfer -> surfer.getName().equals(name));
+    }
+
+    public String getMostExperiencedSurfer() {
+
+        if (surfers.isEmpty()) {
+            return "There are no surfers.";
         }
-        return false;
+
+        Surfer mostExperiencedSurfer = surfers.stream().max(Comparator.comparing(Surfer::getExperience)).get();
+
+        return String.format("%s is most experienced surfer with %d years experience.",
+                mostExperiencedSurfer.getName(), mostExperiencedSurfer.getExperience());
+    }
+
+    public Surfer getSurfer(String name) {
+        return surfers.stream()
+                .filter(surfer -> surfer.getName().equals(name))
+                .findFirst()
+                .orElse(null);
+    }
+
+    public int getCount() {
+        return surfers.size();
+    }
+
+    public String getSurfersWithPersonalSurfBoards() {
+
+        if (surfers.isEmpty()) {
+            return "There are no surfers.";
+        }
+
+        List<String> surfersWithPersonalSurfBoards = new ArrayList<>();
+        for (Surfer surfer : surfers) {
+            if (surfer.ownsASurfBoard()) {
+                surfersWithPersonalSurfBoards.add(surfer.getName());
+            }
+        }
+
+        return "Surfers who have their own surfboards: " + String.join(", ", surfersWithPersonalSurfBoards);
+    }
+
+    public String getReport() {
+
+
+
+
+
+        return null;
     }
 
 
 
-
 }
+
+
+
+
+
+
+
+
+
