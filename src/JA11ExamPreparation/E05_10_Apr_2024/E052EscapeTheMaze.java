5fb426d66fcac460e680fb8eb303ed0c219656a1
tproykov <tproykov@gmail.com>
2025-01-04 15:36:09 +0000

    Update E052EscapeTheMaze.java

--- src/JA11ExamPreparation/E05_10_Apr_2024/E052EscapeTheMaze.java
+++ src/JA11ExamPreparation/E05_10_Apr_2024/E052EscapeTheMaze.java
@@ -1,4 +1,14 @@
 package JA11ExamPreparation.E05_10_Apr_2024;
 
+import java.util.Scanner;
+
 public class E052EscapeTheMaze {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        
+    }
+
 }
