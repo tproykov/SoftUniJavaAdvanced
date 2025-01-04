c092625382e675eca62e3927b670d21113506010
tproykov <tproykov@gmail.com>
2025-01-04 02:21:36 +0000

    update

--- src/JA11ExamPreparation/E033Surfers/Beach.java
+++ src/JA11ExamPreparation/E033Surfers/Beach.java
@@ -1,4 +1,43 @@
 package JA11ExamPreparation.E033Surfers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Beach {
+
+    private String name;
+    private int surfboardsForRent;
+    private List<Surfer> surfers;
+
+    public Beach(String name, int surfboardsForRent) {
+        this.name = name;
+        this.surfboardsForRent = surfboardsForRent;
+        this.surfers = new ArrayList<>();
+    }
+
+    public void addSurfer(Surfer surfer) {
+
+        if (!surfer.ownsASurfBoard() && surfer.getMoney() < 50) {
+            System.out.println(surfer.getName() + " has not enough money to rent a surfboard.");
+        }
+        else if (surfboardsForRent == 0) {
+            System.out.println("There are no free surfboards.");
+        }
+        else {
+            surfers.add(surfer);
+            surfboardsForRent--;
+        }
+    }
+
+    public boolean removeSurfer(String name) {
+        if (surfers.contains(name)) {
+            surfers.remove(name);
+            return true;
+        }
+        return false;
+    }
+
+
+
+
 }
--- src/JA11ExamPreparation/E033Surfers/Surfer.java
+++ src/JA11ExamPreparation/E033Surfers/Surfer.java
@@ -16,6 +16,46 @@ public class Surfer {
         this.money = money;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getExperience() {
+        return experience;
+    }
+
+    public void setExperience(int experience) {
+        this.experience = experience;
+    }
+
+    public boolean ownsASurfBoard() {
+        return ownsASurfBoard;
+    }
+
+    public void setOwnsASurfBoard(boolean ownsASurfBoard) {
+        this.ownsASurfBoard = ownsASurfBoard;
+    }
+
+    public int getMoney() {
+        return money;
+    }
+
+    public void setMoney(int money) {
+        this.money = money;
+    }
+
     @Override
     public String toString() {
         return String.format("Surfer %s is %d years old and has %d years surfing experience.", name, age, experience);
