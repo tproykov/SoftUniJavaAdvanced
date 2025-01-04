b21637f79d6cd33b417880634f87ccfd213d89b4
tproykov <tproykov@gmail.com>
2025-01-04 19:04:50 +0000

    Update E061ChickenSnack.java

--- src/JA11ExamPreparation/E06_17_Feb_2024/E061ChickenSnack.java
+++ src/JA11ExamPreparation/E06_17_Feb_2024/E061ChickenSnack.java
@@ -1,4 +1,13 @@
 package JA11ExamPreparation.E06_17_Feb_2024;
 
+import java.util.Scanner;
+
 public class E061ChickenSnack {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        
+    }
 }
