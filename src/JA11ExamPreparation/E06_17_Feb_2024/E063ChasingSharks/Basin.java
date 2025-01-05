edbe5cae938538a2a66ec5dc4ddc9685afa1cbb3
tproykov <tproykov@gmail.com>
2025-01-05 13:20:23 +0000

    Update Basin.java

--- src/JA11ExamPreparation/E06_17_Feb_2024/E063ChasingSharks/Basin.java
+++ src/JA11ExamPreparation/E06_17_Feb_2024/E063ChasingSharks/Basin.java
@@ -1,6 +1,7 @@
 package JA11ExamPreparation.E06_17_Feb_2024.E063ChasingSharks;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class Basin {
@@ -38,6 +39,34 @@ public class Basin {
     public void setSharks(List<Shark> sharks) {
         this.sharks = sharks;
     }
+
+    public void addShark(Shark shark) {
+        if (this.capacity > this.sharks.size()) {
+            this.sharks.add(shark);
+        }
+        else {
+            System.out.println("This basin is at full capacity!");
+        }
+    }
+
+    public boolean removeShark(String kind) {
+        return sharks.removeIf(shark -> shark.getKind().equals(kind));
+    }
+
+    public Shark getLargestShark() {
+
+        if (sharks.isEmpty()) {
+            return null;
+        }
+
+        Shark LargestShark = sharks.stream()
+                .max(Comparator.comparing(Shark::getLength))
+                .orElse(null);
+
+        return LargestShark;
+    }
+
+    
 
 
 }
