eea26f89b211589e0127f03ef253ab084332f5f1
tproykov <tproykov@gmail.com>
2025-01-06 09:53:16 +0000

    Update E082FishingCompetition.java

--- src/JA11ExamPreparation/E08_21_Oct_2023/E082FishingCompetition.java
+++ src/JA11ExamPreparation/E08_21_Oct_2023/E082FishingCompetition.java
@@ -1,4 +1,42 @@
 package JA11ExamPreparation.E08_21_Oct_2023;
 
+import java.util.Scanner;
+
 public class E082FishingCompetition {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int matrixSize = Integer.parseInt(scanner.nextLine());
+
+        char[][] matrix = new char[matrixSize][matrixSize];
+        int boatRowPosition = 0;
+        int boatColPosition = 0;
+
+        for (int row = 0; row < matrixSize; row++) {
+            String line = scanner.nextLine();
+            for (int col = 0; col < matrixSize; col++) {
+                matrix[row][col] = line.charAt(col);
+                if (matrix[row][col] == 'S') {
+                    boatRowPosition = row;
+                    boatColPosition = col;
+                    matrix[row][col] = '-';
+                }
+            }
+        }
+
+        String command;
+        while (!"collect the nets".equals(command = scanner.nextLine())) {
+
+
+        }
+
+
+    }
+
+
 }
+
+
+
