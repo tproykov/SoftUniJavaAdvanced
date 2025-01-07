93f0992d0d04142a544109f7d177b46980e17b6e
tproykov <tproykov@gmail.com>
2025-01-07 20:44:24 +0000

    Create E02.java

---
+++ src/JA12Exam/E02.java
@@ -0,0 +1,15 @@
+package JA12Exam;
+
+import java.util.Scanner;
+
+public class E02 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        
+
+    }
+
+}
