839de8728068dac2e4e014f450a7328c5d965a84
tproykov <tproykov@gmail.com>
2025-01-07 20:43:59 +0000

    Create E01.java

---
+++ src/JA12Exam/E01.java
@@ -0,0 +1,14 @@
+package JA12Exam;
+
+import java.util.Scanner;
+
+public class E01 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+
+}
