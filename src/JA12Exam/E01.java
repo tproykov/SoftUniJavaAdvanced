97ed963f12b5e0fdacca62660637272c6bafcce3
tproykov <tproykov@gmail.com>
2025-01-07 20:48:53 +0000

    update

--- src/JA12Exam/E01.java
+++ src/JA12Exam/E01.java
@@ -1,12 +1,26 @@
 package JA12Exam;
 
+import java.util.ArrayDeque;
+import java.util.Arrays;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class E01 {
 
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
+
+        ArrayDeque<Integer> queue = Arrays.stream(scanner.nextLine().split("\\s+"))
+                .map(Integer::parseInt)
+                .collect(Collectors.toCollection(ArrayDeque::new));
+
+        ArrayDeque<Integer> stack = Arrays.stream(scanner.nextLine().split("\\s+"))
+                .map(Integer::parseInt)
+                .collect(Collectors.toCollection(ArrayDeque::new));
+
+
+
 
 
     }
