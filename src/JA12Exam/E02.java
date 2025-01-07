fae09c7a7f5e89e2fc7e9899a387d88b64c5e2c7
tproykov <tproykov@gmail.com>
2025-01-07 20:51:04 +0000

    Update E02.java

--- src/JA12Exam/E02.java
+++ src/JA12Exam/E02.java
@@ -8,6 +8,25 @@ public class E02 {
 
         Scanner scanner = new Scanner(System.in);
 
+        int matrixSize = Integer.parseInt(scanner.nextLine());
+
+        char[][] matrix = new char[matrixSize][matrixSize];
+        int playerRowPosition = 0;
+        int playerColPosition = 0;
+
+        for (int row = 0; row < matrixSize; row++) {
+            String line = scanner.nextLine();
+            for (int col = 0; col < matrixSize; col++) {
+                matrix[row][col] = line.charAt(col);
+                if (matrix[row][col] == 'X') {
+                    playerRowPosition = row;
+                    playerColPosition = col;
+                    matrix[row][col] = 'O';
+                }
+            }
+
+        }
+
 
 
     }
