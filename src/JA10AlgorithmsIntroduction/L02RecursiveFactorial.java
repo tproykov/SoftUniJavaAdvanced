7d340971cb1400e241bc50ebb33b74eb1d2a5158
tproykov <tproykov@gmail.com>
2025-01-10 22:48:08 +0000

    update

--- src/JA10AlgorithmsIntroduction/L02RecursiveFactorial.java
+++ src/JA10AlgorithmsIntroduction/L02RecursiveFactorial.java
@@ -1,4 +1,23 @@
 package JA10AlgorithmsIntroduction;
 
+import java.util.Scanner;
+
 public class L02RecursiveFactorial {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int number = Integer.parseInt(scanner.nextLine());
+
+        System.out.println(recursiveFactorial(number));
+    }
+
+    public static int recursiveFactorial(int number) {
+
+        if (number == 0) {
+            return 1;
+        }
+        return number * recursiveFactorial(number - 1);
+    }
 }
