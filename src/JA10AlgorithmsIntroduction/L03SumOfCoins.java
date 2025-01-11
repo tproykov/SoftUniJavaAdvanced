8e1c383ebd4af5b5396d699ac39cdbcdaff92dfb
tproykov <tproykov@gmail.com>
2025-01-11 12:19:42 +0000

    update all

--- src/JA10AlgorithmsIntroduction/L03SumOfCoins.java
+++ src/JA10AlgorithmsIntroduction/L03SumOfCoins.java
@@ -1,8 +1,6 @@
 package JA10AlgorithmsIntroduction;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 
 public class L03SumOfCoins {
 
@@ -12,36 +10,54 @@ public class L03SumOfCoins {
 
         String[] elements = scanner.nextLine().substring(7).split(", ");
 
-        int[] coins = new int[elements.length];
+        int[] availableCoins = new int[elements.length];
 
-        for (int i = 0; i < coins.length; i++) {
-            coins[i] = Integer.parseInt(elements[i]);
+        for (int i = 0; i < availableCoins.length; i++) {
+            availableCoins[i] = Integer.parseInt(elements[i]);
         }
 
         int targetSum = Integer.parseInt(scanner.nextLine().substring(5));
 
-        Map<Integer, Integer> usedCoins = chooseCoins(coins, targetSum);
+        Map<Integer, Integer> usedCoins = chooseCoins(availableCoins, targetSum);
 
+        int returnedSum = usedCoins.entrySet().stream()
+                .mapToInt(entry -> entry.getKey() * entry.getValue())
+                .sum();
 
+        if (targetSum != returnedSum) {
+            System.out.println("Error");
+        }
+        else {
+            System.out.printf("Number of coins to take: %d%n",
+                    usedCoins.values().stream().mapToInt(Integer::intValue).sum());
 
-        for (Map.Entry<Integer, Integer> usedCoin : usedCoins.entrySet()) {
-            System.out.println(usedCoin.getKey() + " coin(s) with value " + usedCoin.getValue());
+            for (Map.Entry<Integer, Integer> usedCoin : usedCoins.entrySet()) {
+                if (usedCoin.getValue() > 0) {
+                    System.out.println(usedCoin.getValue() + " coin(s) with value " + usedCoin.getKey());
+                }
+            }
         }
     }
 
-    public static Map<Integer, Integer> chooseCoins(int[] coins, int targetSum) {
+    public static Map<Integer, Integer> chooseCoins(int[] availableCoins, int targetSum) {
 
-        Map<Integer, Integer> usedCoins = new HashMap<>();
+        // TODO
+        //throw new IllegalArgumentException();
 
-        for (int coin : coins) {
+        Arrays.sort(availableCoins);
 
-            if (coin == targetSum) {
-                usedCoins.put(coin, 1);
-            }
-            else {
-                int countOfCurrentCoin = targetSum / coin;
-            }
-            usedCoins.put(coin, usedCoins.getOrDefault(coin, 0) + 1);
+        Map<Integer, Integer> usedCoins = new LinkedHashMap<>();
+        int coinIndex = availableCoins.length - 1;
+
+        while (targetSum > 0 && coinIndex >= 0) {
+
+            int currentCoin = availableCoins[coinIndex];
+            int countCurrentCoins = targetSum /currentCoin;
+
+            usedCoins.put(currentCoin, countCurrentCoins);
+            targetSum -= countCurrentCoins * currentCoin;
+
+            coinIndex--;
         }
 
         return usedCoins;
