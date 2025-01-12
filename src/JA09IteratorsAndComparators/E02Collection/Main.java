dd2a383f02775177095aef14bad2ecb9064dcff7
tproykov <tproykov@gmail.com>
2025-01-12 23:01:26 +0000

    update

--- src/JA09IteratorsAndComparators/E02Collection/Main.java
+++ src/JA09IteratorsAndComparators/E02Collection/Main.java
@@ -1,4 +1,13 @@
 package JA09IteratorsAndComparators.E02Collection;
 
+import java.util.Scanner;
+
 public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
 }
