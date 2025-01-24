d6c86b5e30ec9b1849dee76d29b60b0a7321714b
tproykov <tproykov@gmail.com>
2025-01-24 21:24:12 +0000

    Update E01MonsterExtermination.java

--- src/JA11ExamPreparation/E10_09_Aug_2023/E01MonsterExtermination.java
+++ src/JA11ExamPreparation/E10_09_Aug_2023/E01MonsterExtermination.java
@@ -1,4 +1,31 @@
 package JA11ExamPreparation.E10_09_Aug_2023;
 
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
 public class E01MonsterExtermination {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        ArrayDeque<Integer> monstersArmour = Arrays.stream(scanner.nextLine().split("\\s+"))
+                .mapToInt(Integer::parseInt)
+                .boxed()
+                .collect(Collectors.toCollection(ArrayDeque::new));
+
+        ArrayDeque<Integer> strikingImpacts = Arrays.stream(scanner.nextLine().split(","))
+                .mapToInt(Integer::parseInt)
+                .boxed()
+                .collect(Collectors.toCollection(ArrayDeque::new));
+
+
+        
+
+
+    }
+
+
 }
