8b9c9453a3fc6e3e56ccf5e11afbe308e351515e
tproykov <tproykov@gmail.com>
2025-01-24 23:08:24 +0000

    update

--- src/JA11ExamPreparation/E10_09_Aug_2023/E01MonsterExtermination.java
+++ src/JA11ExamPreparation/E10_09_Aug_2023/E01MonsterExtermination.java
@@ -1,47 +1,41 @@
 package JA11ExamPreparation.E10_09_Aug_2023;
 
-import java.util.ArrayDeque;
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class E01MonsterExtermination {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        ArrayDeque<Integer> monstersArmour = Arrays.stream(scanner.nextLine().split(","))
+        Queue<Integer> monstersArmour = Arrays.stream(scanner.nextLine().split(","))
                 .mapToInt(Integer::parseInt)
                 .boxed()
-                .collect(Collectors.toCollection(ArrayDeque::new));
+                .collect(Collectors.toCollection(LinkedList::new));
 
-        ArrayDeque<Integer> strikingImpacts = Arrays.stream(scanner.nextLine().split(","))
+        Stack<Integer> strikingImpacts = Arrays.stream(scanner.nextLine().split(","))
                 .mapToInt(Integer::parseInt)
                 .boxed()
-                .collect(Collectors.toCollection(ArrayDeque::new));
+                .collect(Collectors.toCollection(Stack::new));
 
         int totalMonstersKilled = 0;
         while (!monstersArmour.isEmpty() && !strikingImpacts.isEmpty()) {
-            int firstMonsterArmour = monstersArmour.peekFirst();
-            int lastStrikingImpacts = strikingImpacts.peekLast();
+            int firstMonsterArmour = monstersArmour.poll();
+            int lastStrikingImpacts = strikingImpacts.pop();
 
             if (lastStrikingImpacts >= firstMonsterArmour) {
                 totalMonstersKilled++;
                 int remainingStrikingImpact = lastStrikingImpacts - firstMonsterArmour;
-                monstersArmour.pollFirst();
-                strikingImpacts.pollLast();
                 if (remainingStrikingImpact > 0) {
                     if (!strikingImpacts.isEmpty()) {
-                        remainingStrikingImpact += strikingImpacts.pollLast();
+                        remainingStrikingImpact += strikingImpacts.pop();
 
                     }
-                    strikingImpacts.offerLast(remainingStrikingImpact);
+                    strikingImpacts.push(remainingStrikingImpact);
                 }
             }
             else {
                 int remainingMonsterArmour = firstMonsterArmour - lastStrikingImpacts;
-                monstersArmour.pollFirst();
-                strikingImpacts.pollLast();
-                monstersArmour.offerLast(remainingMonsterArmour);
+                monstersArmour.offer(remainingMonsterArmour);
             }
         }
 
