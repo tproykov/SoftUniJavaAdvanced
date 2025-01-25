3f87007eaa84fcdfa775058e58104a7dad7fdb92
tproykov <tproykov@gmail.com>
2025-01-25 23:04:39 +0000

    Update E02BlindMansBuff.java

--- src/JA11ExamPreparation/E12_18_Feb_2023/E02BlindMansBuff.java
+++ src/JA11ExamPreparation/E12_18_Feb_2023/E02BlindMansBuff.java
@@ -1,4 +1,15 @@
 package JA11ExamPreparation.E12_18_Feb_2023;
 
+import java.util.Scanner;
+
 public class E02BlindMansBuff {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        
+
+    }
+
 }
