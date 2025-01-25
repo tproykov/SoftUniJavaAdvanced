215834c0016d9b17130db8e7c6fef21ccfc10425
tproykov <tproykov@gmail.com>
2025-01-25 21:37:54 +0000

    Update E01ApocalypsePreparation.java

--- src/JA11ExamPreparation/E12_18_Feb_2023/E01ApocalypsePreparation.java
+++ src/JA11ExamPreparation/E12_18_Feb_2023/E01ApocalypsePreparation.java
@@ -1,6 +1,7 @@
 package JA11ExamPreparation.E12_18_Feb_2023;
 
-import java.util.Scanner;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class E01ApocalypsePreparation {
 
@@ -8,8 +9,76 @@ public class E01ApocalypsePreparation {
 
         Scanner scanner = new Scanner(System.in);
 
+        ArrayDeque<Integer> textiles = Arrays.stream(scanner.nextLine().split(" "))
+                .map(Integer::parseInt)
+                .collect(Collectors.toCollection(ArrayDeque::new));
+
+        ArrayDeque<Integer> medicines = Arrays.stream(scanner.nextLine().split(" "))
+                .map(Integer::parseInt)
+                .collect(Collectors.toCollection(ArrayDeque::new));
+
+        Map<Integer, String> table = new HashMap<>();
+        table.put(30, "Patch");
+        table.put(40, "Bandages");
+        table.put(100, "MedKit");
+
+        Map<String, Integer> items = new HashMap<>();
+
+        while (!medicines.isEmpty() && !textiles.isEmpty()) {
+
+            int currentTextile = textiles.peekFirst();
+            int currentMedicine = medicines.peekLast();
+
+            int healingItem = currentTextile + currentMedicine;
+
+            if (table.containsKey(healingItem)) {
+                if (!items.containsKey(table.get(healingItem))) {
+                    items.put(table.get(healingItem), 0);
+                }
+                items.put(table.get(healingItem), items.get(table.get(healingItem)) + 1);
+                textiles.pollFirst();
+                medicines.pollLast();
+            }
+            else {
+                if (healingItem > 100) {
+                    textiles.pollFirst();
+                    medicines.pollLast();
+                    int remainder = healingItem - 100;
+                    if (!medicines.isEmpty()) {
+                        int tempValue = remainder + medicines.pollLast();
+                        medicines.offerLast(tempValue);
+                    }
+                    else {
+                        medicines.offerLast(remainder);
+                    }
+                }
+                else {
+                    textiles.pollFirst();
+                    medicines.pollLast();
+                    int tempValue = currentMedicine + 10;
+                    medicines.offerLast(tempValue);
+                }
+
+
+            }
+
+
+
+        }
+
+        if (textiles.isEmpty() && medicines.isEmpty()) {
+            System.out.println("Textiles and medicaments are both empty.");
+        }
+        else if (textiles.isEmpty()) {
+            System.out.println("Textiles are empty.");
+        }
+        else {
+            System.out.println("Medicaments are empty.");
+        }
+
+
+
 
 
     }
-
 }
