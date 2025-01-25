2106f6510848713983c122fa542f6f404ff7078a
tproykov <tproykov@gmail.com>
2025-01-25 15:35:48 +0000

    Update E02TheSquirrel.java

--- src/JA11ExamPreparation/E11_12_Apr_2023/E02TheSquirrel.java
+++ src/JA11ExamPreparation/E11_12_Apr_2023/E02TheSquirrel.java
@@ -25,12 +25,15 @@ public class E02TheSquirrel {
                 if (field[i][j] == 's') {
                     squirrelRowPosition = i;
                     squirrelColPosition = j;
+                    field[i][j] = '*';
                 }
             }
         }
 
         int hazelnutsCollected = 0;
         for (String command : commands) {
+
+            field[squirrelRowPosition][squirrelColPosition] = '*';
 
             switch (command) {
                 case "up" -> squirrelRowPosition--;
@@ -39,20 +42,39 @@ public class E02TheSquirrel {
                 case "right" -> squirrelColPosition++;
             }
 
+            if (isOutOfBounds(fieldSize, squirrelRowPosition, squirrelColPosition)) {
+                System.out.println("The squirrel is out of the field.");
+                System.out.println("Hazelnuts collected: " + hazelnutsCollected);
+                return;
+            }
 
+            if (field[squirrelRowPosition][squirrelColPosition] == 't') {
+                System.out.println("Unfortunately, the squirrel stepped on a trap...");
+                System.out.println("Hazelnuts collected: " + hazelnutsCollected);
+                return;
+            }
 
-            
+            if (field[squirrelRowPosition][squirrelColPosition] == 'h') {
+                hazelnutsCollected++;
+                field[squirrelRowPosition][squirrelColPosition] = '*';
+                if (hazelnutsCollected == 3) {
+                    field[squirrelRowPosition][squirrelColPosition] = 's';
+                    System.out.println("Good job! You have collected all hazelnuts!");
+                    System.out.println("Hazelnuts collected: " + hazelnutsCollected);
+                    return;
+                }
+            }
 
-
-
+            field[squirrelRowPosition][squirrelColPosition] = 's';
         }
 
+        System.out.println("There are more hazelnuts to collect.");
+        System.out.println("Hazelnuts collected: " + hazelnutsCollected);
+    }
 
+    public static boolean isOutOfBounds(int fieldSize, int squirrelRowPosition, int squirrelColPosition) {
 
-
-
-        }
-
-
+        return squirrelRowPosition >= fieldSize || squirrelColPosition >= fieldSize || squirrelRowPosition < 0
+                || squirrelColPosition < 0;
     }
 }
