bdfd796630fd89291b66bedfcee6a6cc06bdbd8e
tproykov <tproykov@gmail.com>
2025-01-25 13:47:42 +0000

    Update E01RubberDuckDebuggers.java

--- src/JA11ExamPreparation/E11_12_Apr_2023/E01RubberDuckDebuggers.java
+++ src/JA11ExamPreparation/E11_12_Apr_2023/E01RubberDuckDebuggers.java
@@ -1,4 +1,15 @@
 package JA11ExamPreparation.E11_12_Apr_2023;
 
+import java.util.Scanner;
+
 public class E01RubberDuckDebuggers {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+
+
+    }
+
 }
