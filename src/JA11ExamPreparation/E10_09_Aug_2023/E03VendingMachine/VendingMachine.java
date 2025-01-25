06bd289ca5d892e18565fa06962d7ad1e3094e2c
tproykov <tproykov@gmail.com>
2025-01-25 12:52:37 +0000

    Update VendingMachine.java

--- src/JA11ExamPreparation/E10_09_Aug_2023/E03VendingMachine/VendingMachine.java
+++ src/JA11ExamPreparation/E10_09_Aug_2023/E03VendingMachine/VendingMachine.java
@@ -1,6 +1,7 @@
 package JA11ExamPreparation.E10_09_Aug_2023.E03VendingMachine;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class VendingMachine {
@@ -41,8 +42,15 @@ public class VendingMachine {
 
     public boolean removeDrink(String name) {
         return this.drinks.removeIf(drink -> drink.getName().equals(name));
-
     }
+
+    public Drink getLongest() {
+        return this.drinks.stream()
+                .max(Comparator.comparingInt(Drink::getVolume))
+                .orElse(null);
+    }
+
+
 
 
 
