44d149f4f2852cd987a83a61bd7a982349669f3e
tproykov <tproykov@gmail.com>
2025-01-26 00:51:20 +0000

    Update E02BlindMansBuff.java

--- src/JA11ExamPreparation/E12_18_Feb_2023/E02BlindMansBuff.java
+++ src/JA11ExamPreparation/E12_18_Feb_2023/E02BlindMansBuff.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 public class E02BlindMansBuff {
 
     public static void main(String[] args) {
-
         Scanner scanner = new Scanner(System.in);
 
         int rows = scanner.nextInt();
@@ -13,14 +12,13 @@ public class E02BlindMansBuff {
         scanner.nextLine();
 
         char[][] playGround = new char[rows][cols];
-
         int playerRowPosition = 0;
         int playerColPosition = 0;
 
         for (int i = 0; i < rows; i++) {
-            char[] row = scanner.nextLine().replace(" ", "").toCharArray();
+            String[] row = scanner.nextLine().split(" ");
             for (int j = 0; j < cols; j++) {
-                playGround[i][j] = row[j];
+                playGround[i][j] = row[j].charAt(0);
                 if (playGround[i][j] == 'B') {
                     playerRowPosition = i;
                     playerColPosition = j;
@@ -31,11 +29,10 @@ public class E02BlindMansBuff {
         int peopleTouched = 0;
         int movesMade = 0;
         String command;
-        while (!"Finish".equals(command = scanner.nextLine())) {
 
+        while (!"Finish".equals(command = scanner.nextLine())) {
             int playerRowOldPosition = playerRowPosition;
             int playerColOldPosition = playerColPosition;
-            playGround[playerRowOldPosition][playerColOldPosition] = '-';
 
             switch (command) {
                 case "up" -> playerRowPosition--;
@@ -47,42 +44,38 @@ public class E02BlindMansBuff {
             if (isOutOfBounds(rows, cols, playerRowPosition, playerColPosition)) {
                 playerRowPosition = playerRowOldPosition;
                 playerColPosition = playerColOldPosition;
+                continue;
             }
 
             if (playGround[playerRowPosition][playerColPosition] == 'O') {
                 playerRowPosition = playerRowOldPosition;
                 playerColPosition = playerColOldPosition;
+                continue;
             }
-            else if (playGround[playerRowPosition][playerColPosition] == '-') {
-                movesMade++;
-            }
-            else if (playGround[playerRowPosition][playerColPosition] == 'P') {
+
+            playGround[playerRowOldPosition][playerColOldPosition] = '-';
+
+            if (playGround[playerRowPosition][playerColPosition] == 'P') {
                 peopleTouched++;
-                movesMade++;
-                if (peopleTouched == 3) {
-                    playGround[playerRowPosition][playerColPosition] = 'B';
-                    break;
-                }
             }
+
+            if (playGround[playerRowPosition][playerColPosition] != 'O') {
+                movesMade++;
+            }
+
             playGround[playerRowPosition][playerColPosition] = 'B';
+
+            if (peopleTouched == 3) {
+                break;
+            }
         }
 
         System.out.println("Game over!");
-        System.out.println("Touched opponents: " + peopleTouched + " Moves made: " + movesMade);
+        System.out.printf("Touched opponents: %d Moves made: %d%n", peopleTouched, movesMade);
     }
 
     private static boolean isOutOfBounds(int rows, int cols, int playerRowPosition, int playerColPosition) {
         return playerRowPosition >= rows || playerColPosition >= cols || playerRowPosition < 0
                 || playerColPosition < 0;
-    }
-
-    private static void printPlayGround(char[][] playGround) {
-
-        for (int i = 0; i < playGround.length; i++) {
-            for (int j = 0; j < playGround[i].length; j++) {
-                System.out.print(playGround[i][j] + "");
-            }
-            System.out.println();
-        }
     }
 }
