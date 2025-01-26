b3e03710c9a6d106e239d21655cd10d2f0ee8026
tproykov <tproykov@gmail.com>
2025-01-26 14:34:13 +0000

    Update E01EnergyDrinks.java

--- src/JA11ExamPreparation/E14_22_Oct_2022/E01EnergyDrinks.java
+++ src/JA11ExamPreparation/E14_22_Oct_2022/E01EnergyDrinks.java
@@ -1,4 +1,15 @@
 package JA11ExamPreparation.E14_22_Oct_2022;
 
+import java.util.Scanner;
+
 public class E01EnergyDrinks {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+
+
+
+    }
 }
