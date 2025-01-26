e1c660482fab3a3c5827d5eceb5616e1508e7ac7
tproykov <tproykov@gmail.com>
2025-01-26 00:58:59 +0000

    update

---
+++ src/JA11ExamPreparation/E12_18_Feb_2023/E03SoftUniKnderGarten/Main.java
@@ -0,0 +1,15 @@
+package JA11ExamPreparation.E12_18_Feb_2023.E03SoftUniKnderGarten;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        
+
+    }
+
+}
