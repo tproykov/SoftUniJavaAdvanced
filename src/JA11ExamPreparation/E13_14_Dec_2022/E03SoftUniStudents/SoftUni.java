f38cab8acb003588d79aac66dfa966b554ca58d5
tproykov <tproykov@gmail.com>
2025-01-26 14:02:00 +0000

    Update SoftUni.java

--- src/JA11ExamPreparation/E13_14_Dec_2022/E03SoftUniStudents/SoftUni.java
+++ src/JA11ExamPreparation/E13_14_Dec_2022/E03SoftUniStudents/SoftUni.java
@@ -1,4 +1,35 @@
 package JA11ExamPreparation.E13_14_Dec_2022.E03SoftUniStudents;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SoftUni {
+
+    private int capacity;
+    private List<Student> data;
+
+    public SoftUni(int capacity) {
+        this.capacity = capacity;
+        this.data = new ArrayList<>();
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public List<Student> getData() {
+        return data;
+    }
+
+    public void setData(List<Student> data) {
+        this.data = data;
+    }
+
+
+
+    
 }
