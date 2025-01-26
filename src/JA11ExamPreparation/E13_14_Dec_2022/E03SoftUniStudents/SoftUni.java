5aea746ea4e78678b5eafa5672dd12cf494f1816
tproykov <tproykov@gmail.com>
2025-01-26 14:20:51 +0000

    Update SoftUni.java

--- src/JA11ExamPreparation/E13_14_Dec_2022/E03SoftUniStudents/SoftUni.java
+++ src/JA11ExamPreparation/E13_14_Dec_2022/E03SoftUniStudents/SoftUni.java
@@ -29,7 +29,49 @@ public class SoftUni {
         this.data = data;
     }
 
+    public int getCount() {
+        return data.size();
+    }
 
+    public String insert(Student student) {
+        if (this.capacity > this.data.size() && !this.data.contains(student)) {
+            this.data.add(student);
+            return "Added student " + student.getFirstName() + " " + student.getLastName() + ".";
+        }
+        else if (this.data.contains(student)) {
+            return "Student is already in the hall.";
+        }
+        else if (this.capacity == this.data.size()) {
+            return "The hall is full.";
+        }
+        return null;
+    }
 
-    
+    public String remove(Student student) {
+        if (!this.data.contains(student)) {
+            return "Student not found.";
+        }
+        else {
+            this.data.remove(student);
+            return "Removed student " + student.getFirstName() + " " + student.getLastName() + ".";
+        }
+    }
+
+    public Student getStudent(String firstName, String lastName) {
+        for (Student student : data) {
+            if (student.getFirstName().equals(firstName) && student.getLastName().equals(lastName)) {
+                return student;
+            }
+        }
+        return null;
+    }
+
+    public String getStatistics() {
+
+        StringBuilder statistics = new StringBuilder();
+        statistics.append("Hall size: ").append(this.capacity).append("\n");
+        data.forEach(student -> statistics.append(student.toString()).append("\n"));
+
+        return statistics.toString();
+    }
 }
