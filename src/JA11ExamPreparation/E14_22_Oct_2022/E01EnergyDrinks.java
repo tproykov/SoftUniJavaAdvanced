3f2a028d5f9e9d7b93bfc546def71c9d7317a84f
tproykov <tproykov@gmail.com>
2025-01-27 23:33:42 +0000

    Update E01EnergyDrinks.java

--- src/JA11ExamPreparation/E14_22_Oct_2022/E01EnergyDrinks.java
+++ src/JA11ExamPreparation/E14_22_Oct_2022/E01EnergyDrinks.java
@@ -1,6 +1,9 @@
 package JA11ExamPreparation.E14_22_Oct_2022;
 
+import java.util.ArrayDeque;
+import java.util.Arrays;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class E01EnergyDrinks {
 
@@ -8,8 +11,44 @@ public class E01EnergyDrinks {
 
         Scanner scanner = new Scanner(System.in);
 
+        ArrayDeque<Integer> caffeine = Arrays.stream(scanner.nextLine().split(", "))
+                .map(Integer::parseInt)
+                .collect(Collectors.toCollection(ArrayDeque::new));
 
+        ArrayDeque<Integer> energyDrinks = Arrays.stream(scanner.nextLine().split(", "))
+                .map(Integer::parseInt)
+                .collect(Collectors.toCollection(ArrayDeque::new));
 
+        int caffeineConsumed = 0;
 
+        while (!caffeine.isEmpty() && !energyDrinks.isEmpty()) {
+
+            int currentCaffeine = caffeine.peekLast();
+            int currentEnergyDrink = energyDrinks.peekFirst();
+
+            int caffeineInDrink = currentCaffeine * currentEnergyDrink;
+
+            if (caffeineInDrink + caffeineConsumed <= 300) {
+                caffeineConsumed += caffeineInDrink;
+                energyDrinks.pollFirst();
+                caffeine.pollLast();
+            }
+            else {
+                caffeine.pollLast();
+                energyDrinks.pollFirst();
+                energyDrinks.offerLast(currentEnergyDrink);
+                caffeineConsumed -= 30;
+                if (caffeineConsumed < 0) {
+                    caffeineConsumed = 0;
+                }
+            }
+        }
+
+        if (!energyDrinks.isEmpty()) {
+            System.out.print("Drinks left: ");
+            System.out.println(energyDrinks.stream()
+                    .map(String::valueOf)
+                    .collect(Collectors.joining(", ")));
+        }
     }
 }
