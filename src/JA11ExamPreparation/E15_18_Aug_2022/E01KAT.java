1c94a8ae69c03e870b4733e16da58f3b7384ea43
tproykov <tproykov@gmail.com>
2025-01-29 22:53:10 +0000

    Update E01KAT.java

--- src/JA11ExamPreparation/E15_18_Aug_2022/E01KAT.java
+++ src/JA11ExamPreparation/E15_18_Aug_2022/E01KAT.java
@@ -1,4 +1,21 @@
 package JA11ExamPreparation.E15_18_Aug_2022;
 
+import java.util.Scanner;
+
 public class E01KAT {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+
+
+    }
+
+
+
 }
+
+
+
+
