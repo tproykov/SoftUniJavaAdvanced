7d7dd713b3bb7675807986b24e3fd8a4afc3dccb
tproykov <tproykov@gmail.com>
2025-01-29 22:49:32 +0000

    update

--- src/JA11ExamPreparation/E14_22_Oct_2022/E03AnimalShelter/Shelter.java
+++ src/JA11ExamPreparation/E14_22_Oct_2022/E03AnimalShelter/Shelter.java
@@ -62,7 +62,7 @@ public class Shelter {
         StringBuilder statistics = new StringBuilder();
         statistics.append("The shelter has the following animals:\n");
         for (Animal animal : data) {
-            statistics.append(animal.getName()).append(" ").append(animal.getCaretaker());
+            statistics.append(animal.getName()).append(" ").append(animal.getCaretaker()).append("\n");
         }
         return statistics.toString();
     }
