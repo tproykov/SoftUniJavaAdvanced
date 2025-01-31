35dd355c6e4ba33eeb5f954f5c8f1dc2d95ec318
tproykov <tproykov@gmail.com>
2025-01-31 02:29:03 +0000

    Update E01KAT.java

--- src/JA11ExamPreparation/E15_18_Aug_2022/E01KAT.java
+++ src/JA11ExamPreparation/E15_18_Aug_2022/E01KAT.java
@@ -1,12 +1,23 @@
 package JA11ExamPreparation.E15_18_Aug_2022;
 
+import java.util.ArrayDeque;
+import java.util.Arrays;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class E01KAT {
 
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
+
+        ArrayDeque<Integer> licensePlates = Arrays.stream(scanner.nextLine().split(", "))
+                .map(Integer::parseInt)
+                .collect(Collectors.toCollection(ArrayDeque::new));
+
+        ArrayDeque<Integer> cars = Arrays.stream(scanner.nextLine().split(", "))
+                .map(Integer::parseInt)
+                .collect(Collectors.toCollection(ArrayDeque::new));
 
 
 
