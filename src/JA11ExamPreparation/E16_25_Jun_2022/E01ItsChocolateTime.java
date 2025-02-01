7fa7c03231626727ee8f42556aeabc0b9be63bf9
tproykov <tproykov@gmail.com>
2025-02-01 18:44:18 +0000

    Update E01ItsChocolateTime.java

--- src/JA11ExamPreparation/E16_25_Jun_2022/E01ItsChocolateTime.java
+++ src/JA11ExamPreparation/E16_25_Jun_2022/E01ItsChocolateTime.java
@@ -1,4 +1,15 @@
 package JA11ExamPreparation.E16_25_Jun_2022;
 
+import java.util.Scanner;
+
 public class E01ItsChocolateTime {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+
+
+    }
+
 }
