4cd4e2a0b14695fe63abe56147081f9bf39de6d5
tproykov <tproykov@gmail.com>
2025-02-01 22:54:58 +0000

    update

--- src/JA11ExamPreparation/E16_25_Jun_2022/E02StickyFingers.java
+++ src/JA11ExamPreparation/E16_25_Jun_2022/E02StickyFingers.java
@@ -20,11 +20,51 @@ public class E02StickyFingers {
         int thiefRowPosition = thiefPosition[0];
         int thiefColPosition = thiefPosition[1];
 
+        int moneyStolen = 0;
+        boolean goneToJail = false;
+        for (String direction : directions) {
 
+            int newRowPosition = thiefRowPosition;
+            int newColPosition = thiefColPosition;
+            field[newRowPosition][newColPosition] = '+';
 
+            switch (direction) {
+                case "up" -> newRowPosition--;
+                case "down" -> newRowPosition++;
+                case "left" -> newColPosition--;
+                case "right" -> newColPosition++;
+            }
 
+            if (isOutOfBound(fieldSize, newRowPosition, newColPosition)) {
+                System.out.println("You cannot leave the town, there is police outside!");
+            }
+            else {
+                thiefRowPosition = newRowPosition;
+                thiefColPosition = newColPosition;
+            }
 
+            if (field[thiefRowPosition][thiefColPosition] == '$') {
+                int currentMoneyStolen = thiefRowPosition * thiefColPosition;
+                moneyStolen += currentMoneyStolen;
+                System.out.println("You successfully stole " + currentMoneyStolen + "$.");
+                field[thiefRowPosition][thiefColPosition] = '+';
+            }
+            else if (field[thiefRowPosition][thiefColPosition] == 'P') {
+                goneToJail = true;
+                break;
+            }
+        }
 
+        if (goneToJail) {
+            field[thiefRowPosition][thiefColPosition] = '#';
+            System.out.println("You got caught with " + moneyStolen + "$, and you are going to jail.");
+            printField(field);
+        }
+        else {
+            field[thiefRowPosition][thiefColPosition] = 'D';
+            System.out.println("Your last theft has finished successfully with " + moneyStolen + "$ in your pocket.");
+            printField(field);
+        }
     }
 
     private static void populateField(char[][] field, Scanner scanner) {
@@ -54,8 +94,11 @@ public class E02StickyFingers {
 
     private static void printField(char[][] field) {
         for (char[] chars : field) {
-            for (char aChar : chars) {
-                System.out.print(aChar + " ");
+            for (int row = 0; row < field.length; row++) {
+                System.out.print(chars[row]);
+                if (row < field.length - 1) {
+                    System.out.print(" ");
+                }
             }
             System.out.println();
         }
