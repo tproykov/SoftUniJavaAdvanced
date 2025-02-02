9a4f94ae2a59cb24a89cfc4af3967c33e12aa9eb
tproykov <tproykov@gmail.com>
2025-02-02 21:58:31 +0000

    Update E02Armory.java

--- src/JA11ExamPreparation/E17_13_Apr_2022/E02Armory.java
+++ src/JA11ExamPreparation/E17_13_Apr_2022/E02Armory.java
@@ -1,4 +1,15 @@
 package JA11ExamPreparation.E17_13_Apr_2022;
 
+import java.util.Scanner;
+
 public class E02Armory {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        
+
+    }
+
 }
