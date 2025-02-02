92885711168cba11682e104a99a31cd3b8d2b452
tproykov <tproykov@gmail.com>
2025-02-02 20:48:54 +0000

    Update E01Blacksmith.java

--- src/JA11ExamPreparation/E17_13_Apr_2022/E01Blacksmith.java
+++ src/JA11ExamPreparation/E17_13_Apr_2022/E01Blacksmith.java
@@ -1,12 +1,25 @@
 package JA11ExamPreparation.E17_13_Apr_2022;
 
+import java.util.ArrayDeque;
+import java.util.Arrays;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class E01Blacksmith {
 
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
+
+        ArrayDeque<Integer> steel = Arrays.stream(scanner.nextLine().split(" "))
+                .map(Integer::parseInt)
+                .collect(Collectors.toCollection(ArrayDeque::new));
+
+        ArrayDeque<Integer> coal = Arrays.stream(scanner.nextLine().split(" "))
+                .map(Integer::parseInt)
+                .collect(Collectors.toCollection(ArrayDeque::new));
+
+        
 
 
 
