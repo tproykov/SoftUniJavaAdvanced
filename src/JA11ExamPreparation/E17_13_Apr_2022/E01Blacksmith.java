835348e41544cb1a070bee11409f646665ca7ef8
tproykov <tproykov@gmail.com>
2025-02-02 20:28:21 +0000

    update

--- src/JA11ExamPreparation/E17_13_Apr_2022/E01Blacksmith.java
+++ src/JA11ExamPreparation/E17_13_Apr_2022/E01Blacksmith.java
@@ -1,4 +1,16 @@
 package JA11ExamPreparation.E17_13_Apr_2022;
 
+import java.util.Scanner;
+
 public class E01Blacksmith {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        
+
+    }
+
+
 }
