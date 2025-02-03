dd34f0d21f0ac5086be8b906881594c7ce7dc111
tproykov <tproykov@gmail.com>
2025-02-03 13:08:25 +0000

    update

--- src/JA11ExamPreparation/E17_13_Apr_2022/E03EasterBasket/Basket.java
+++ src/JA11ExamPreparation/E17_13_Apr_2022/E03EasterBasket/Basket.java
@@ -1,4 +1,59 @@
 package JA11ExamPreparation.E17_13_Apr_2022.E03EasterBasket;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Basket {
+
+    private String material;
+    private int capacity;
+    private List<Egg> data;
+
+    public Basket(String material, int capacity) {
+        this.material = material;
+        this.capacity = capacity;
+        this.data = new ArrayList<>();
+    }
+
+    public String getMaterial() {
+        return material;
+    }
+
+    public void setMaterial(String material) {
+        this.material = material;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public List<Egg> getData() {
+        return data;
+    }
+
+    public void setData(List<Egg> data) {
+        this.data = data;
+    }
+
+    public void addEgg(Egg egg) {
+        if (this.capacity > this.data.size()) {
+            this.data.add(egg);
+        }
+    }
+
+    public boolean removeEgg(String color) {
+        return this.data.removeIf(egg -> egg.getColor().equals(color));
+    }
+
+
+
+
+
+
+
+
 }
--- src/JA11ExamPreparation/E17_13_Apr_2022/E03EasterBasket/Main.java
+++ src/JA11ExamPreparation/E17_13_Apr_2022/E03EasterBasket/Main.java
@@ -2,36 +2,39 @@ package JA11ExamPreparation.E17_13_Apr_2022.E03EasterBasket;
 
 public class Main {
 
-    //Initialize the repository (Basket)
-    Basket basket = new Basket("Wood", 20);
-    //Initialize entity (Egg)
-    Egg egg = new Egg("Red", 10, "oval");
+    public static void main(String[] args) {
+
+        //Initialize the repository (Basket)
+        Basket basket = new Basket("Wood", 20);
+        //Initialize entity (Egg)
+        Egg egg = new Egg("Red", 10, "oval");
 //Print Egg
-System.out.println(egg);  //Red egg, 10 strength, oval shape
+        System.out.println(egg);  //Red egg, 10 strength, oval shape
 
 //Add Egg
-basket.addEgg(egg);
+        basket.addEgg(egg);
 
 //Remove Egg
-System.out.println(basket.removeEgg("Pink"));  //False
+        System.out.println(basket.removeEgg("Pink"));  //False
 
-    Egg secondEgg = new Egg("Green", 9, "pointy");
+        Egg secondEgg = new Egg("Green", 9, "pointy");
 
 //Add Egg
-basket.addEgg(secondEgg);
+        basket.addEgg(secondEgg);
 
-    //Get strongest egg
-    Egg strongestEgg = basket.getStrongestEgg();
+        //Get strongest egg
+        Egg strongestEgg = basket.getStrongestEgg();
 
-System.out.println(strongestEgg);
+        System.out.println(strongestEgg);
 
-    //Get egg
-    Egg getEgg = basket.getEgg("Green"); //Green egg with 9 strength, pointy shape
-System.out.println(getEgg);
+        //Get egg
+        Egg getEgg = basket.getEgg("Green"); //Green egg with 9 strength, pointy shape
+        System.out.println(getEgg);
 
 
-System.out.println(basket.report());
+        System.out.println(basket.report());
 //Wood basket contains:
 //Red egg, with 10 strength and oval shape.
 //Green egg, with 9 strength and pointy shape.
+    }
 }
