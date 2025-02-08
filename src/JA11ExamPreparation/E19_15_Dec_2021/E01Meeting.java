cb4feb557198286d16c07e3dceff6b0c06eefcae
tproykov <tproykov@gmail.com>
2025-02-08 11:41:46 +0000

    Update E01Meeting.java

--- src/JA11ExamPreparation/E19_15_Dec_2021/E01Meeting.java
+++ src/JA11ExamPreparation/E19_15_Dec_2021/E01Meeting.java
@@ -1,4 +1,54 @@
 package JA11ExamPreparation.E19_15_Dec_2021;
 
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.Scanner;
+
 public class E01Meeting {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        Deque<Integer> males = new ArrayDeque<>();
+        Arrays.stream(scanner.nextLine().split("\\s+"))
+                .map(Integer::parseInt)
+                .forEach(males::push);
+
+        Deque<Integer> females = new ArrayDeque<>();
+        Arrays.stream(scanner.nextLine().split("\\s+"))
+                .map(Integer::parseInt)
+                .forEach(females::offer);
+
+        while (!males.isEmpty() && !females.isEmpty()) {
+
+            int currentMale = males.pop();
+            if (currentMale <= 0) {
+                currentMale = males.pop();
+            }
+
+
+            int currentFemale = females.poll();
+            if (currentFemale <= 0) {
+                currentFemale = females.poll();
+            }
+
+
+            
+            if (currentMale != currentFemale) {
+                currentMale -= 2;
+                males.push(currentMale);
+            }
+        }
+
+
+
+
+
+
+
+
+
+    }
 }
