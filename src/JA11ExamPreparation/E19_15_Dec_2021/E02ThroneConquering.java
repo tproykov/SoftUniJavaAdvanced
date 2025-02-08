a5c0a7eb8cefff40041b48ce0ba073bf9ca261d4
tproykov <tproykov@gmail.com>
2025-02-08 12:43:39 +0000

    Update E02ThroneConquering.java

--- src/JA11ExamPreparation/E19_15_Dec_2021/E02ThroneConquering.java
+++ src/JA11ExamPreparation/E19_15_Dec_2021/E02ThroneConquering.java
@@ -1,4 +1,14 @@
 package JA11ExamPreparation.E19_15_Dec_2021;
 
+import java.util.Scanner;
+
 public class E02ThroneConquering {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+
 }
