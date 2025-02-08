ac96f9199151d426cb572f851b1dd5b837c95256
tproykov <tproykov@gmail.com>
2025-02-08 23:07:46 +0000

    update

--- src/JA11ExamPreparation/E20_23_Oct_2021/E02MouseAndCheese.java
+++ src/JA11ExamPreparation/E20_23_Oct_2021/E02MouseAndCheese.java
@@ -17,21 +17,66 @@ public class E02MouseAndCheese {
         int[] mousePosition = findMousePosition(territory);
         int mouseRowPosition = mousePosition[0];
         int mouseColumnPosition = mousePosition[1];
-        territory[mouseRowPosition][mouseColumnPosition] = '-';
 
+        int cheeseEaten = 0;
+        boolean outOfBounds = false;
         String command;
-        while (!"End".equals(command = scanner.nextLine())) {
+        while (!"end".equals(command = scanner.nextLine())) {
 
+            territory[mouseRowPosition][mouseColumnPosition] = '-';
 
+            switch (command) {
+                case "up" -> mouseRowPosition--;
+                case "down" -> mouseRowPosition++;
+                case "left" -> mouseColumnPosition--;
+                case "right" -> mouseColumnPosition++;
+            }
+
+            if (isOutOfBound(territory, mouseRowPosition, mouseColumnPosition)) {
+                outOfBounds = true;
+                break;
+            }
             
+            if (territory[mouseRowPosition][mouseColumnPosition] == 'c') {
+                cheeseEaten++;
+            }
+            else if (territory[mouseRowPosition][mouseColumnPosition] == 'B') {
+                territory[mouseRowPosition][mouseColumnPosition] = '-';
+                switch (command) {
+                    case "up" -> mouseRowPosition--;
+                    case "down" -> mouseRowPosition++;
+                    case "left" -> mouseColumnPosition--;
+                    case "right" -> mouseColumnPosition++;
+                }
+                if (isOutOfBound(territory, mouseRowPosition, mouseColumnPosition)) {
+                    outOfBounds = true;
+                    break;
+                }
+                if (territory[mouseRowPosition][mouseColumnPosition] == 'c') {
+                    cheeseEaten++;
+                }
+            }
 
-
-
-
-
+            territory[mouseRowPosition][mouseColumnPosition] = 'M';
         }
 
+        if (!outOfBounds) {
+            territory[mouseRowPosition][mouseColumnPosition] = 'M';
+        }
 
+        if (outOfBounds) {
+            System.out.println("Where is the mouse?");
+        }
+
+        if (cheeseEaten < 5) {
+            System.out.println("The mouse couldn't eat the cheeses, she needed " + (5 - cheeseEaten)
+                    + " cheeses more.");
+        }
+        else {
+            System.out.println("Great job, the mouse is fed " + cheeseEaten + " cheeses!");
+        }
+
+        printTerritory(territory);
     }
 
     private static void fillTerritory(char[][] territory, Scanner scanner) {
@@ -56,10 +101,15 @@ public class E02MouseAndCheese {
         return mousePosition;
     }
 
-    private static void printTerritory(char[][] territory, Scanner scanner) {
-        for (int i = 0; i < territory.length; i++) {
-            for (int j = 0; j < territory[i].length; j++) {
-                System.out.print(territory[i][j]);
+    private static boolean isOutOfBound(char[][] territory, int mouseRowPosition, int mouseColumnPosition) {
+        return mouseRowPosition < 0 || mouseRowPosition >= territory.length || mouseColumnPosition < 0
+                || mouseColumnPosition >= territory[mouseRowPosition].length;
+    }
+
+    private static void printTerritory(char[][] territory) {
+        for (char[] chars : territory) {
+            for (char aChar : chars) {
+                System.out.print(aChar);
             }
             System.out.println();
         }
