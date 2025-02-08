3ce6aba113120593659577c2d190514b913bd70b
tproykov <tproykov@gmail.com>
2025-02-08 22:25:15 +0000

    update

--- src/JA11ExamPreparation/E20_23_Oct_2021/E01AutumnCocktails.java
+++ src/JA11ExamPreparation/E20_23_Oct_2021/E01AutumnCocktails.java
@@ -18,52 +18,24 @@ public class E01AutumnCocktails {
                 .map(Integer::parseInt)
                 .forEach(freshnessLevels::push);
 
-        Map<String, Integer> cocktailsProduced = new TreeMap<>();
-        cocktailsProduced.put("Pear Sour", 0);
-        cocktailsProduced.put("The Harvest", 0);
-        cocktailsProduced.put("Apple Hinny", 0);
-        cocktailsProduced.put("High Fashion", 0);
+        Map<String, Integer> cocktailsProduced = mapProducedCocktails();
 
-        Map<String, Integer> cocktailTable = new HashMap<>();
-        cocktailTable.put("Pear Sour", 150);
-        cocktailTable.put("The Harvest", 250);
-        cocktailTable.put("Apple Hinny", 300);
-        cocktailTable.put("High Fashion", 400);
+        Map<String, Integer> cocktailTable = populateCocktailTable();
+
+        mixCocktails(freshnessLevels, ingredients, cocktailTable, cocktailsProduced);
 
         boolean fourCocktailsDone = true;
-        while (!freshnessLevels.isEmpty() && !ingredients.isEmpty()) {
-
-            int currentIngredient = ingredients.poll();
-            if (currentIngredient == 0) {
-                continue;
-            }
-            int currentFreshnessLevel = freshnessLevels.pop();
-            int totalFreshnessLevels = currentFreshnessLevel * currentIngredient;
-
-            for (Map.Entry<String, Integer> entry : cocktailTable.entrySet()) {
-
-                if (entry.getValue() == totalFreshnessLevels) {
-
-                    String cocktailProduced = entry.getKey();
-                    cocktailsProduced.put(cocktailProduced, cocktailsProduced.get(cocktailProduced) + 1);
-                }
-                else {
-                    currentIngredient += 5;
-                    ingredients.offer(currentIngredient);
-                }
-            }
-
-            for (Map.Entry<String, Integer> entry : cocktailsProduced.entrySet()) {
-                if (entry.getValue() == 0) {
-                    fourCocktailsDone = false;
-                }
-            }
-
-            if (fourCocktailsDone) {
-                break;
+        for (Map.Entry<String, Integer> entry : cocktailsProduced.entrySet()) {
+            if (entry.getValue() == 0) {
+                fourCocktailsDone = false;
             }
         }
 
+        printResults(fourCocktailsDone, ingredients, cocktailsProduced);
+    }
+
+    private static void printResults(boolean fourCocktailsDone, Deque<Integer> ingredients,
+                                     Map<String, Integer> cocktailsProduced) {
         if (fourCocktailsDone) {
             System.out.println("It's party time! The cocktails are ready!");
         }
@@ -81,7 +53,55 @@ public class E01AutumnCocktails {
         }
 
         for (Map.Entry<String, Integer> entry : cocktailsProduced.entrySet()) {
-            System.out.println(" # " + entry.getKey() + " --> " + entry.getValue());
+            if (entry.getValue() > 0) {
+                System.out.println(" # " + entry.getKey() + " --> " + entry.getValue());
+            }
         }
+    }
+
+    private static void mixCocktails(Deque<Integer> freshnessLevels, Deque<Integer> ingredients,
+                                     Map<String, Integer> cocktailTable, Map<String, Integer> cocktailsProduced) {
+        while (!freshnessLevels.isEmpty() && !ingredients.isEmpty()) {
+
+            int currentIngredient = ingredients.poll();
+            if (currentIngredient == 0) {
+                continue;
+            }
+            int currentFreshnessLevel = freshnessLevels.pop();
+            int totalFreshnessLevels = currentFreshnessLevel * currentIngredient;
+
+            boolean cocktailMixed = false;
+            for (Map.Entry<String, Integer> entry : cocktailTable.entrySet()) {
+
+                if (entry.getValue() == totalFreshnessLevels) {
+                    cocktailMixed = true;
+                    String cocktailProduced = entry.getKey();
+                    cocktailsProduced.put(cocktailProduced, cocktailsProduced.get(cocktailProduced) + 1);
+                }
+            }
+
+            if (!cocktailMixed) {
+                currentIngredient += 5;
+                ingredients.offer(currentIngredient);
+            }
+        }
+    }
+
+    private static Map<String, Integer> mapProducedCocktails() {
+        Map<String, Integer> cocktailsProduced = new TreeMap<>();
+        cocktailsProduced.put("Pear Sour", 0);
+        cocktailsProduced.put("The Harvest", 0);
+        cocktailsProduced.put("Apple Hinny", 0);
+        cocktailsProduced.put("High Fashion", 0);
+        return cocktailsProduced;
+    }
+
+    private static Map<String, Integer> populateCocktailTable() {
+        Map<String, Integer> cocktailTable = new HashMap<>();
+        cocktailTable.put("Pear Sour", 150);
+        cocktailTable.put("The Harvest", 250);
+        cocktailTable.put("Apple Hinny", 300);
+        cocktailTable.put("High Fashion", 400);
+        return cocktailTable;
     }
 }
