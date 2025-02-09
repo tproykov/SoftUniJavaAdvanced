45bfdb5664cc21a0f6e627b7e2801a599bbed036
tproykov <tproykov@gmail.com>
2025-02-09 11:46:31 +0000

    update

--- src/JA11ExamPreparation/E21_18_Aug_2021/E01PastryShop.java
+++ src/JA11ExamPreparation/E21_18_Aug_2021/E01PastryShop.java
@@ -18,18 +18,29 @@ public class E01PastryShop {
                 .map(Integer::parseInt)
                 .forEach(ingredients::push);
 
-        Map<String, Integer> foodsCooked = new TreeMap<>();
-        foodsCooked.put("Biscuit", 0);
-        foodsCooked.put("Cake", 0);
-        foodsCooked.put("Pastry", 0);
-        foodsCooked.put("Pie", 0);
+        Map<String, Integer> foodTable = defineFoodTable();
 
-        Map<String, Integer> foodTable = new HashMap<>();
-        foodTable.put("Biscuit", 25);
-        foodTable.put("Cake", 50);
-        foodTable.put("Pastry", 75);
-        foodTable.put("Pie", 100);
+        Map<String, Integer> foodsCooked = trackCookedFoods();
 
+        cookFoods(liquids, ingredients, foodTable, foodsCooked);
+
+        boolean allFoodsCooked = checkIfAllCooked(foodsCooked);
+
+        printResults(allFoodsCooked, liquids, ingredients, foodsCooked);
+    }
+
+    private static boolean checkIfAllCooked(Map<String, Integer> foodsCooked) {
+        boolean allFoodsCooked = true;
+        for (Map.Entry<String, Integer> entry : foodsCooked.entrySet()) {
+            if (entry.getValue() == 0) {
+                allFoodsCooked = false;
+                break;
+            }
+        }
+        return allFoodsCooked;
+    }
+
+    private static void cookFoods(Deque<Integer> liquids, Deque<Integer> ingredients, Map<String, Integer> foodTable, Map<String, Integer> foodsCooked) {
         while (!liquids.isEmpty() && !ingredients.isEmpty()) {
 
             int currentLiquid = liquids.poll();
@@ -49,8 +60,53 @@ public class E01PastryShop {
                 ingredients.push(currentIngredient);
             }
         }
+    }
 
+    private static Map<String, Integer> trackCookedFoods() {
+        Map<String, Integer> foodsCooked = new LinkedHashMap<>();
+        foodsCooked.put("Biscuit", 0);
+        foodsCooked.put("Cake", 0);
+        foodsCooked.put("Pie", 0);
+        foodsCooked.put("Pastry", 0);
+        return foodsCooked;
+    }
 
+    private static Map<String, Integer> defineFoodTable() {
+        Map<String, Integer> foodTable = new HashMap<>();
+        foodTable.put("Biscuit", 25);
+        foodTable.put("Cake", 50);
+        foodTable.put("Pastry", 75);
+        foodTable.put("Pie", 100);
+        return foodTable;
+    }
 
+    private static void printResults(boolean allFoodsCooked, Deque<Integer> liquids, Deque<Integer> ingredients,
+                                     Map<String, Integer> foodsCooked) {
+        if (allFoodsCooked) {
+            System.out.println("Great! You succeeded in cooking all the food!");
+        }
+        else {
+            System.out.println("What a pity! You didn't have enough materials to cook everything.");
+        }
+
+        System.out.print("Liquids left: ");
+        if (liquids.isEmpty()) {
+            System.out.println("none");
+        }
+        else {
+            System.out.println(liquids.toString().replaceAll("[\\[\\]]", ""));
+        }
+
+        System.out.print("Ingredients left: ");
+        if (ingredients.isEmpty()) {
+            System.out.println("none");
+        }
+        else {
+            System.out.println(ingredients.toString().replaceAll("[\\[\\]]", ""));
+        }
+
+        for (Map.Entry<String, Integer> entry : foodsCooked.entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
     }
 }
