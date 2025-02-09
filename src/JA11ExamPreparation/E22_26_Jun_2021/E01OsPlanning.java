565f370b4101933740b96ddc64e55b78a1897555
tproykov <tproykov@gmail.com>
2025-02-09 14:46:54 +0000

    Update E01OsPlanning.java

--- src/JA11ExamPreparation/E22_26_Jun_2021/E01OsPlanning.java
+++ src/JA11ExamPreparation/E22_26_Jun_2021/E01OsPlanning.java
@@ -1,5 +1,8 @@
 package JA11ExamPreparation.E22_26_Jun_2021;
 
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Deque;
 import java.util.Scanner;
 
 public class E01OsPlanning {
@@ -8,6 +11,34 @@ public class E01OsPlanning {
 
         Scanner scanner = new Scanner(System.in);
 
+        Deque<Integer> tasks = new ArrayDeque<>();
+        Arrays.stream(scanner.nextLine().split("\\s+"))
+                .mapToInt(Integer::parseInt)
+                .forEach(tasks::push);
 
+        Deque<Integer> threads = new ArrayDeque<>();
+        Arrays.stream(scanner.nextLine().split("\\s+"))
+                .mapToInt(Integer::parseInt)
+                .forEach(tasks::offer);
+
+        int taskToKill = Integer.parseInt(scanner.nextLine());
+
+        int taskKiller;
+        while (true) {
+
+            int currentTask = tasks.pop();
+            int currentThread = threads.poll();
+
+            if (currentThread < currentTask) {
+                tasks.push(currentTask);
+            }
+            else if (taskToKill == currentTask) {
+                taskKiller = currentThread;
+                break;
+            }
+        }
+
+        System.out.println("Thread with value " + taskKiller + " killed task " + taskToKill);
+        System.out.println(String.join(" ", threads.toString()));
     }
 }
