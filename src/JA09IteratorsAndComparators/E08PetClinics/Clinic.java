c88424d3fb7fb5b146fbb9a702067d78582d961b
tproykov <tproykov@gmail.com>
2025-02-11 20:56:14 +0000

    update

---
+++ src/JA09IteratorsAndComparators/E08PetClinics/Clinic.java
@@ -0,0 +1,82 @@
+package JA09IteratorsAndComparators.E08PetClinics;
+
+public class Clinic {
+    private String name;
+    private Pet[] rooms;
+
+    public Clinic(String name, int roomsCount) {
+        this.name = name;
+
+        if (roomsCount % 2 == 0) {
+            throw new IllegalArgumentException("Invalid Operation!");
+        }
+        this.rooms = new Pet[roomsCount];
+    }
+
+    public boolean add(Pet pet) {
+        int n = this.rooms.length;
+        int center = n / 2; // zero-indexed center
+        for (int i = 0; i < n; i++) {
+            int index;
+
+            if (i % 2 == 0) {
+                index = center + (i / 2);
+            } else {
+                index = center - ((i + 1) / 2);
+            }
+            if (rooms[index] == null) {
+                rooms[index] = pet;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean release() {
+        int n = this.rooms.length;
+        int center = n / 2;
+
+        for (int i = center; i < n; i++) {
+            if (rooms[i] != null) {
+                rooms[i] = null;
+                return true;
+            }
+        }
+
+        for (int i = 0; i < center; i++) {
+            if (rooms[i] != null) {
+                rooms[i] = null;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean hasEmptyRooms() {
+        for (Pet pet : rooms) {
+            if (pet == null) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void printRoom(int roomNumber) {
+        int index = roomNumber - 1;
+        if (rooms[index] == null) {
+            System.out.println("Room empty");
+        } else {
+            System.out.println(rooms[index].toString());
+        }
+    }
+
+    public void printAll() {
+        for (Pet pet : rooms) {
+            if (pet == null) {
+                System.out.println("Room empty");
+            } else {
+                System.out.println(pet.toString());
+            }
+        }
+    }
+}
--- src/JA09IteratorsAndComparators/E08PetClinics/Main.java
+++ src/JA09IteratorsAndComparators/E08PetClinics/Main.java
@@ -1,4 +1,97 @@
 package JA09IteratorsAndComparators.E08PetClinics;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int nCommands = Integer.parseInt(scanner.nextLine());
+
+        Map<String, Pet> pets = new HashMap<>();
+        Map<String, Clinic> clinics = new HashMap<>();
+
+        for (int i = 0; i < nCommands; i++) {
+            String line = scanner.nextLine();
+            String[] tokens = line.split("\\s+");
+            String command = tokens[0];
+
+            try {
+                switch (command) {
+                    case "Create":
+
+                        if (tokens[1].equals("Pet")) {
+
+                            String petName = tokens[2];
+                            int petAge = Integer.parseInt(tokens[3]);
+                            String petKind = tokens[4];
+                            Pet pet = new Pet(petName, petAge, petKind);
+                            pets.put(petName, pet);
+                        } else if (tokens[1].equals("Clinic")) {
+
+                            String clinicName = tokens[2];
+                            int roomsCount = Integer.parseInt(tokens[3]);
+                            Clinic clinic = new Clinic(clinicName, roomsCount);
+                            clinics.put(clinicName, clinic);
+                        }
+                        break;
+
+                    case "Add":
+
+                        String petName = tokens[1];
+                        String clinicName = tokens[2];
+
+                        if (!pets.containsKey(petName) || !clinics.containsKey(clinicName)) {
+                            throw new IllegalArgumentException();
+                        }
+                        boolean addResult = clinics.get(clinicName).add(pets.get(petName));
+                        System.out.println(addResult);
+                        break;
+
+                    case "Release":
+
+                        clinicName = tokens[1];
+                        if (!clinics.containsKey(clinicName)) {
+                            throw new IllegalArgumentException();
+                        }
+                        boolean releaseResult = clinics.get(clinicName).release();
+                        System.out.println(releaseResult);
+                        break;
+
+                    case "HasEmptyRooms":
+
+                        clinicName = tokens[1];
+                        if (!clinics.containsKey(clinicName)) {
+                            throw new IllegalArgumentException();
+                        }
+                        boolean emptyRooms = clinics.get(clinicName).hasEmptyRooms();
+                        System.out.println(emptyRooms);
+                        break;
+
+                    case "Print":
+
+                        clinicName = tokens[1];
+                        if (!clinics.containsKey(clinicName)) {
+                            throw new IllegalArgumentException();
+                        }
+                        Clinic clinic = clinics.get(clinicName);
+                        if (tokens.length == 2) {
+                            clinic.printAll();
+                        } else if (tokens.length == 3) {
+                            int roomNumber = Integer.parseInt(tokens[2]);
+                            clinic.printRoom(roomNumber);
+                        }
+                        break;
+
+                    default:
+                        break;
+                }
+            } catch (IllegalArgumentException ex) {
+                System.out.println("Invalid Operation!");
+            }
+        }
+
+        scanner.close();
+    }
 }
