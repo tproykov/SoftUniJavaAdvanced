88c2041000de0edf43fc0f2966ee08ec9d027ea1
tproykov <tproykov@gmail.com>
2025-02-12 21:57:30 +0000

    Update ListyIterator.java

--- src/JA09IteratorsAndComparators/E02Collection/ListyIterator.java
+++ src/JA09IteratorsAndComparators/E02Collection/ListyIterator.java
@@ -1,35 +1,66 @@
-package JA09IteratorsAndComparators.E02Collection;
-
 import java.util.List;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
 
-public class ListyIterator<T> {
-
+public class ListyIterator<T> implements Iterator<T>, Iterable<T> {
     private List<T> elements;
     private int currentIndex;
 
+    @SafeVarargs
     public ListyIterator(T... elements) {
-        this.elements = List.of(elements);
+        this.elements = new ArrayList<>();
+        for (T element : elements) {
+            this.elements.add(element);
+        }
+        this.currentIndex = 0;
+    }
+
+    public ListyIterator(List<T> elements) {
+        this.elements = new ArrayList<>(elements);
         this.currentIndex = 0;
     }
 
     public boolean move() {
         if (hasNext()) {
-            this.currentIndex++;
+            currentIndex++;
             return true;
         }
         return false;
     }
 
+    @Override
     public boolean hasNext() {
-        return this.currentIndex < this.elements.size() - 1;
+        return currentIndex < elements.size() - 1;
+    }
+
+    @Override
+    public T next() {
+        if (!hasNext()) {
+            throw new NoSuchElementException();
+        }
+        return elements.get(currentIndex++);
     }
 
     public void print() {
-        if (this.elements.isEmpty()) {
+        if (elements.isEmpty()) {
             throw new IllegalStateException("Invalid Operation!");
         }
-        else {
-            System.out.println(this.elements.get(this.currentIndex));
+        System.out.println(elements.get(currentIndex));
+    }
+
+    public void printAll() {
+        if (elements.isEmpty()) {
+            throw new IllegalStateException("Invalid Operation!");
         }
+        String result = String.join(" ", elements.stream()
+                .map(Object::toString)
+                .toArray(String[]::new));
+        System.out.println(result);
+    }
+    
+    @Override
+    public Iterator<T> iterator() {
+        return this;
     }
 }
