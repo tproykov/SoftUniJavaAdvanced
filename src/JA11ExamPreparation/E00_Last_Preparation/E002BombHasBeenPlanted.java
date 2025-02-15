022abbf8c155818225c425b9bb34d34e17ed804a
tproykov <tproykov@gmail.com>
2025-02-15 00:51:24 +0000

    Update E002BombHasBeenPlanted.java

--- src/JA11ExamPreparation/E00_Last_Preparation/E002BombHasBeenPlanted.java
+++ src/JA11ExamPreparation/E00_Last_Preparation/E002BombHasBeenPlanted.java
@@ -1,5 +1,6 @@
 package JA11ExamPreparation.E00_Last_Preparation;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class E002BombHasBeenPlanted {
@@ -8,7 +9,58 @@ public class E002BombHasBeenPlanted {
 
         Scanner scanner = new Scanner(System.in);
 
-        
+        int[] matrixDimensions = Arrays.stream(scanner.nextLine().split(", "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+        int matrixRows = matrixDimensions[0];
+        int matrixColumns = matrixDimensions[1];
 
+        char[][] matrix = new char[matrixRows][matrixColumns];
+
+        fillMatrix(matrix, matrixRows, matrixColumns, scanner);
+
+        int[] counterTerroristPosition = findCounterTerroristPosition(matrix);
+        int counterTerroristRowPosition = counterTerroristPosition[0];
+        int counterTerroristColumnPosition = counterTerroristPosition[1];
+        matrix[counterTerroristRowPosition][counterTerroristColumnPosition] = '*';
+
+        while (true) {
+
+            String command = scanner.nextLine();
+
+
+            switch (command) {
+                case "up" -> counterTerroristRowPosition--;
+                case "down" -> counterTerroristRowPosition++;
+                case "left" -> counterTerroristColumnPosition--;
+            }
+
+
+
+        }
+
+
+    }
+
+    private static void fillMatrix(char[][] matrix, int matrixRows, int matrixColumns, Scanner scanner) {
+        for (int i = 0; i < matrixRows; i++) {
+            String line = scanner.nextLine();
+            for (int j = 0; j < matrixColumns; j++) {
+                matrix[i][j] = line.charAt(j);
+            }
+        }
+    }
+
+    private static int[] findCounterTerroristPosition(char[][] matrix) {
+        int[] counterTerroristPosition  = new int[2];
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (matrix[i][j] == 'C') {
+                    counterTerroristPosition[0] = i;
+                    counterTerroristPosition[1] = j;
+                }
+            }
+        }
+        return counterTerroristPosition;
     }
 }
