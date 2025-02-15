cc197aec7867d4d338326773ab75f2597c0d310a
tproykov <tproykov@gmail.com>
2025-02-15 00:36:52 +0000

    Update E002BombHasBeenPlanted.java

--- src/JA11ExamPreparation/E00_Last_Preparation/E002BombHasBeenPlanted.java
+++ src/JA11ExamPreparation/E00_Last_Preparation/E002BombHasBeenPlanted.java
@@ -1,4 +1,14 @@
 package JA11ExamPreparation.E00_Last_Preparation;
 
+import java.util.Scanner;
+
 public class E002BombHasBeenPlanted {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        
+
+    }
 }
